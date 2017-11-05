8a00972a140e67af85421fc42664d5e15ee2955f
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-11-05 22:51:10 +0800

    Vector Square Archfiend fix

--- script/c13452889.lua
+++ script/c13452889.lua
@@ -1,6 +1,5 @@
 --ベクター・スケア・デーモン
 --Vector Square Archfiend
---Script by nekrozar
 function c13452889.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
@@ -34,9 +33,8 @@ function c13452889.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local bc=c:GetBattleTarget()
 	local zone1=c:GetLinkedZone(tp)
 	local zone2=c:GetLinkedZone(1-tp)
-	if chk==0 then return c:GetLinkedZone()~=0
-		and (bc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone1,true))
-		or (bc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp,zone2)) end
+	if chk==0 then return c:GetLinkedZone()~=0 and (bc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone1,true) 
+		or bc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp,zone2)) end
 	Duel.SetTargetCard(bc)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,bc,1,0,0)
 end
@@ -48,9 +46,9 @@ function c13452889.spop(e,tp,eg,ep,ev,re,r,rp)
 		local zone2=c:GetLinkedZone(1-tp)
 		if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone1)
 			and (not tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp,zone2) or Duel.SelectYesNo(tp,aux.Stringid(13452889,1))) then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP,zone1)
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP,zone1)
 		else
-			if Duel.SpecialSummon(g,0,tp,1-tp,false,false,POS_FACEUP,zone2)~=0
+			if Duel.SpecialSummon(tc,0,tp,1-tp,false,false,POS_FACEUP,zone2)~=0
 				and c:IsRelateToBattle() then
 				local e1=Effect.CreateEffect(c)
 				e1:SetType(EFFECT_TYPE_SINGLE)
@@ -62,4 +60,3 @@ function c13452889.spop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-
