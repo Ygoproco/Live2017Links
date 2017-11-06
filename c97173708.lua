6cb390862edbe85817fb02416b15bfe7b769c9ef
AlphaKretin <deewhydeeecks@gmail.com>
2017-11-06 15:48:23 +1030

    Fix "Oh Tokenbaum!" (#47)
    * Fix "Oh Tokenbaum!"
    
    SelectUnselect typoes
    
    * fix

---
+++ c97173708.lua
@@ -0,0 +1,100 @@
+--トークン生誕祭
+function c97173708.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetLabel(0)
+	e1:SetCost(c97173708.cost)
+	e1:SetTarget(c97173708.target)
+	e1:SetOperation(c97173708.activate)
+	c:RegisterEffect(e1)
+end
+function c97173708.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	return true
+end
+function c97173708.rfilter(c,e,tp)
+	local lv=c:GetLevel()
+	return lv>0 and c:IsType(TYPE_TOKEN) and Duel.IsExistingTarget(c97173708.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,lv)
+end
+function c97173708.spfilter(c,e,tp,clv)
+	return c:IsLevel(clv) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c97173708.rescon(sg,e,tp,mg)
+	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:GetClassCount(Card.GetLevel)==1
+end
+function c97173708.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c97173708.spfilter(chkc,e,tp,e:GetLabel()) end
+	local rg=Duel.GetReleaseGroup(tp):Filter(c97173708.rfilter,nil,e,tp)
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2 and rg:GetCount()>1 and aux.SelectUnselectGroup(rg,e,tp,2,9,c97173708.rescon,0)
+	end
+	local sg=aux.SelectUnselectGroup(rg,e,tp,2,9,c97173708.rescon,1,tp,HINTMSG_RELEASE)
+	local lv=sg:GetFirst():GetLevel()
+	e:SetLabel(lv)
+	Duel.Release(sg,REASON_COST)
+	local ft=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),sg:GetCount())
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c97173708.spfilter,tp,LOCATION_GRAVE,0,1,ft,nil,e,tp,lv)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,g:GetCount(),0,0)
+end
+function c97173708.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()==0 then return end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	if ft<g:GetCount() then return end
+	local c=e:GetHandler()
+	local fid=c:GetFieldID()
+	local tc=g:GetFirst()
+	while tc do
+		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e2)
+		tc:RegisterFlagEffect(97173708,RESET_EVENT+0x1fe0000,0,1,fid)
+		tc=g:GetNext()
+	end
+	Duel.SpecialSummonComplete()
+	g:KeepAlive()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCountLimit(1)
+	e1:SetLabel(fid)
+	e1:SetLabelObject(g)
+	e1:SetCondition(c97173708.descon)
+	e1:SetOperation(c97173708.desop)
+	Duel.RegisterEffect(e1,tp)
+end
+function c97173708.desfilter(c,fid)
+	return c:GetFlagEffectLabel(97173708)==fid
+end
+function c97173708.descon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(c97173708.desfilter,1,nil,e:GetLabel()) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return true end
+end
+function c97173708.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	local tg=g:Filter(c97173708.desfilter,nil,e:GetLabel())
+	Duel.Destroy(tg,REASON_EFFECT)
+end
