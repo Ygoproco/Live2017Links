d17431625ac09dfa25c5553a7994258354bf6756
mercury233 <me@mercury233.me>
2017-11-18 20:52:07 +0800

    Fixed "Ojamadification"
    for Ash Blossom

--- script/c2390019.lua
+++ script/c2390019.lua
@@ -71,7 +71,7 @@ function c2390019.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c2390019.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,e:GetLabel(),tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,e:GetLabel(),tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK)
 end
 function c2390019.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
