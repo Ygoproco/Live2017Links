3dcea244485e0f7259daf3e10887ee595088b9de
ahtelel <32773013+ahtelel@users.noreply.github.com>
2017-11-24 00:34:46 +0700

    Fix Archfiend's Call (#172)
    Now interacts correctly with cards with on-discard effects

--- script/c97803170.lua
+++ script/c97803170.lua
@@ -64,7 +64,7 @@ function c97803170.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 or not tc:IsRelateToEffect(e) then return end
-	if Duel.DiscardHand(tp,c97803170.cfilter,1,1,REASON_EFFECT,nil)~=0 then
+	if Duel.DiscardHand(tp,c97803170.cfilter,1,1,REASON_EFFECT+REASON_DISCARD,nil)~=0 then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
