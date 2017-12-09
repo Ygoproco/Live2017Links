09466e672a1c549af0f47ecb088f75cdec256615
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-09 00:29:31 -0200

    fix for Mythical Beast Jackal

--- script/c91182675.lua
+++ script/c91182675.lua
@@ -43,7 +43,7 @@ function c91182675.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 function c91182675.ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)
+	return Duel.GetFieldGroupCount(tp,LOCATION_PZONE,0) == 1
 end
 function c91182675.ctfilter(c)
 	return c:IsFaceup() and c:IsCanAddCounter(0x1,1)
