50a772ad19896afea5e61ba5ff636162c64a6d27
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-12 08:06:22 -0200

    Small fixes

---
+++ script/c65079854.lua
@@ -0,0 +1,55 @@
+--憎悪の棘
+function c65079854.initial_effect(c)
+	aux.AddEquipProcedure(c,nil,c65079854.filter)
+	--Atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(600)
+	c:RegisterEffect(e2)
+	--Pierce
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e3)
+	--indes
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetTargetRange(0,LOCATION_MZONE)
+	e5:SetTarget(c65079854.indestg)
+	e5:SetValue(1)
+	c:RegisterEffect(e5)
+	--battle
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(65079854,0))
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e6:SetRange(LOCATION_SZONE)
+	e6:SetCode(EVENT_BATTLED)
+	e6:SetCondition(c65079854.adcon)
+	e6:SetOperation(c65079854.adop)
+	c:RegisterEffect(e6)
+end
+function c65079854.filter(c)
+	return c:IsCode(73580471) or c:IsRace(RACE_PLANT)
+end
+function c65079854.indestg(e,c)
+	return e:GetHandler():GetEquipTarget() and c==e:GetHandler():GetEquipTarget():GetBattleTarget()
+end
+function c65079854.adcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker()==e:GetHandler():GetEquipTarget() and Duel.GetAttackTarget()~=nil
+end
+function c65079854.adop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local bc=Duel.GetAttackTarget()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(-600)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	bc:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	bc:RegisterEffect(e2)
+end
