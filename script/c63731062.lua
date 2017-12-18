7fc2a5f6e1aa27af0eb4fa699bc63ed006734c09
EerieCode <eeriecode@gmail.com>
2017-12-18 12:28:38 +0100

    Official numbers for PP20

--- script/c63731062.lua
+++ script/c63731062.lua
@@ -1,50 +1,50 @@
 --白闘気一角
 --White Aura Monokeros
 --Scripted by Eerie Code
-function c100224004.initial_effect(c)
+function c63731062.initial_effect(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_WATER),1,1,nil,1,99)
 	c:EnableReviveLimit()
 	--special summon fish
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100224004,0))
+	e1:SetDescription(aux.Stringid(63731062,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1,100224004)
-	e1:SetCondition(c100224004.spcon)
-	e1:SetTarget(c100224004.sptg)
-	e1:SetOperation(c100224004.spop)
+	e1:SetCountLimit(1,63731062)
+	e1:SetCondition(c63731062.spcon)
+	e1:SetTarget(c63731062.sptg)
+	e1:SetOperation(c63731062.spop)
 	c:RegisterEffect(e1)
 	--special summon self
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100224004,1))
+	e2:SetDescription(aux.Stringid(63731062,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCondition(c100224004.condition)
-	e2:SetCost(c100224004.cost)
-	e2:SetTarget(c100224004.target)
-	e2:SetOperation(c100224004.operation)
+	e2:SetCondition(c63731062.condition)
+	e2:SetCost(c63731062.cost)
+	e2:SetTarget(c63731062.target)
+	e2:SetOperation(c63731062.operation)
 	c:RegisterEffect(e2)
 end
-function c100224004.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c63731062.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
-function c100224004.spfilter(c,e,tp)
+function c63731062.spfilter(c,e,tp)
 	return c:IsRace(RACE_FISH) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100224004.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100224004.spfilter(chkc,e,tp) end
+function c63731062.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c63731062.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100224004.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c63731062.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100224004.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c63731062.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c100224004.spop(e,tp,eg,ep,ev,re,r,rp)
+function c63731062.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -56,25 +56,25 @@ function c100224004.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 	Duel.SpecialSummonComplete()
 end
-function c100224004.condition(e,tp,eg,ep,ev,re,r,rp)
+function c63731062.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return rp==1-tp and c:IsReason(REASON_DESTROY) and c:IsReason(REASON_BATTLE+REASON_EFFECT)
 end
-function c100224004.cfilter(c,tp)
+function c63731062.cfilter(c,tp)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true) 
 		and (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or (c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5))
 end
-function c100224004.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100224004.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,e:GetHandler(),tp) end
+function c63731062.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c63731062.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,e:GetHandler(),tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c100224004.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,e:GetHandler(),tp)
+	local g=Duel.SelectMatchingCard(tp,c63731062.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,e:GetHandler(),tp)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c100224004.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c63731062.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c100224004.operation(e,tp,eg,ep,ev,re,r,rp)
+function c63731062.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP) then
 		local e1=Effect.CreateEffect(c)
@@ -86,3 +86,4 @@ function c100224004.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummonComplete()
 	end
 end
+
