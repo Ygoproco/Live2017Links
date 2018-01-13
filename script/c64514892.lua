a67dc0bc126a8adf75a9a2c5eaed08be409ee86e
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-01-14 00:50:54 +0100

    Pint-Sized Priest Hidaruma Fix
    Fixed the increase of atk Value

--- script/c64514892.lua
+++ script/c64514892.lua
@@ -39,7 +39,7 @@ function c64514892.cfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST)
 end
 function c64514892.atkval(e,c)
-	return Duel.GetMatchingGroupCount(c64514892.cfilter,0,LOCATION_MZONE,LOCATION_MZONE,nil)*500
+	return Duel.GetMatchingGroupCount(c64514892.cfilter,0,LOCATION_MZONE,LOCATION_MZONE,nil)*100
 end
 function c93966624.desfilter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP)
