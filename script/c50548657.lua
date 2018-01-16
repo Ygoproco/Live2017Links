413c1ae637619012bef2edb74be26d0d5a47c4a7
senpaizuri3 <senpaizuri3@gmail.com>
2018-01-16 11:54:00 -0500

    Fix "Prompt Horn"

--- script/c50548657.lua
+++ script/c50548657.lua
@@ -14,10 +14,10 @@ function c50548657.initial_effect(c)
 	e1:SetOperation(c50548657.spop)
 	c:RegisterEffect(e1)
 end
-function c50548657.costfilter(c,e,tp,ft)
+function c50548657.costfilter(c,e,tp,g,ft)
 	local lv=c:GetLevel()
 	return c:IsLevelBelow(4) and c:IsRace(RACE_CYBERSE) and Duel.GetMZoneCount(tp,c)>0 and (c:IsControler(tp) or c:IsFaceup())
-		and g:CheckWithSumEqual(Card.GetLevel,lv,1,ft)
+		and g:CheckWithSumEqual(Card.GetLevel,lv,1,ft+1)
 end
 function c50548657.spfilter(c,e,tp)
 	return c:IsRace(RACE_CYBERSE) and c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
