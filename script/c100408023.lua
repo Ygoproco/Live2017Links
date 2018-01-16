6a1c5d7f80d80fec3a2850632de24e47825784bb
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-01-16 02:34:28 +0100

    Lafarl, Hero of the Skyfang Brigade Fix
    Fix for the Excavating effect that should not count Lafarl, Hero of the Skyfang Brigade

--- script/c100408023.lua
+++ script/c100408023.lua
@@ -28,7 +28,7 @@ function c100408023.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function c100408023.ctfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x214)
+	return not c:IsCode(c100408023) and c:IsFaceup() and c:IsSetCard(0x214)
 end
 function c100408023.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
