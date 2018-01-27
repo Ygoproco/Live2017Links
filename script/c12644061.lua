5d9624c49ab44a8e25da6a4e409f45b4bc571cbf
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2018-01-27 21:13:55 +0800

    Fixes
    Necrovalley Interaction vs Zombie World/Advanced Dark
    Infinite Transcience negating itself (graphic)

---
+++ script/c12644061.lua
@@ -0,0 +1,108 @@
+--アドバンスド・ダーク
+function c12644061.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--attribute
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e2:SetTarget(c12644061.tg)
+	e2:SetValue(ATTRIBUTE_DARK)
+	c:RegisterEffect(e2)
+	--negate
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCondition(c12644061.discon)
+	e3:SetOperation(c12644061.disop)
+	c:RegisterEffect(e3)
+	--
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(12644061,0))
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCondition(c12644061.damcon)
+	e4:SetCost(c12644061.damcost)
+	e4:SetOperation(c12644061.damop)
+	c:RegisterEffect(e4)
+	--12644061 chk
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(12644061)
+	e5:SetRange(LOCATION_FZONE)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetTargetRange(1,1)
+	e5:SetValue(c12644061.val)
+	c:RegisterEffect(e5)
+end
+function c12644061.tg(e,c)
+	if not c:IsSetCard(0x1034) then return false end
+	if c:GetFlagEffect(1)==0 then
+		c:RegisterFlagEffect(1,0,0,0)
+		local eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
+		c:ResetFlagEffect(1)
+		for _,te in ipairs(eff) do
+			local op=te:GetOperation()
+			if not op or op(e,c) then return false end
+		end
+	end
+	return true
+end
+function c12644061.val(e,c,re,chk)
+	if chk==0 then return c:IsSetCard(0x1034) end
+	return ATTRIBUTE_DARK
+end
+function c12644061.discon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	return at and a:IsSetCard(0x2034)
+end
+function c12644061.disop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttackTarget()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+	tc:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DISABLE_EFFECT)
+	e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+	tc:RegisterEffect(e2)
+end
+function c12644061.damcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	return Duel.GetBattleDamage(tp)>0
+		and ((a:IsControler(tp) and a:IsSetCard(0x1034)) or (at and at:IsControler(tp) and at:IsSetCard(0x1034)))
+end
+function c12644061.dfilter(c)
+	return c:IsSetCard(0x1034) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
+end
+function c12644061.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c12644061.dfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c12644061.dfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c12644061.damop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetOperation(c12644061.dop)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE)
+	c:RegisterEffect(e1)
+end
+function c12644061.dop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(tp,0)
+end
---
+++ script/c4064256.lua
@@ -0,0 +1,57 @@
+--アンデットワールド
+function c4064256.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--race
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetCode(EFFECT_CHANGE_RACE)
+	e2:SetValue(RACE_ZOMBIE)
+	e2:SetTarget(c4064256.tg)
+	c:RegisterEffect(e2)
+	--summon limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCode(EFFECT_CANNOT_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,1)
+	e3:SetTarget(c4064256.sumlimit)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_CANNOT_MSET)
+	c:RegisterEffect(e4)
+	--4064256 chk
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(4064256)
+	e5:SetRange(LOCATION_FZONE)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetTargetRange(1,1)
+	e5:SetValue(c4064256.val)
+	c:RegisterEffect(e5)
+end
+function c4064256.tg(e,c)
+	if c:GetFlagEffect(1)==0 then
+		c:RegisterFlagEffect(1,0,0,0)
+		local eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
+		c:ResetFlagEffect(1)
+		for _,te in ipairs(eff) do
+			local op=te:GetOperation()
+			if not op or op(e,c) then return false end
+		end
+	end
+	return true
+end
+function c4064256.val(e,c,re,chk)
+	if chk==0 then return true end
+	return RACE_ZOMBIE
+end
+function c4064256.sumlimit(e,c,tp,sumtp)
+	return sumtp&SUMMON_TYPE_ADVANCE==SUMMON_TYPE_ADVANCE and not c:IsRace(RACE_ZOMBIE)
+end
