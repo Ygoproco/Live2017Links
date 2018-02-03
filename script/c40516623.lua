ba5957fc985a25b4f22b0e3707b5bcd29247dfdf
EerieCode <eeriecode@gmail.com>
2018-02-03 09:47:46 +0100

    Official numbers - Extreme Force (TCG)

--- script/c40516623.lua
+++ script/c40516623.lua
@@ -1,6 +1,6 @@
 --Yoko-Zuna Sumo Spirit
 --Scripted by Eerie Code
-function c101003000.initial_effect(c)
+function c40516623.initial_effect(c)
 	--pendulum summon
 	aux.EnablePendulumAttribute(c)
 	--spirit return
@@ -11,42 +11,42 @@ function c101003000.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCondition(c101003000.thcon)
-	e1:SetTarget(c101003000.thtg)
-	e1:SetOperation(c101003000.thop)
+	e1:SetCondition(c40516623.thcon)
+	e1:SetTarget(c40516623.thtg)
+	e1:SetOperation(c40516623.thop)
 	c:RegisterEffect(e1)
 	--gy
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetTarget(c101003000.gytg)
-	e2:SetOperation(c101003000.gyop)
+	e2:SetTarget(c40516623.gytg)
+	e2:SetOperation(c40516623.gyop)
 	c:RegisterEffect(e2)
 end
-function c101003000.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c40516623.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(Card.IsSummonType,1,nil,SUMMON_TYPE_PENDULUM)
 end
-function c101003000.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c40516623.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
-function c101003000.thop(e,tp,eg,ep,ev,re,r,rp)
+function c40516623.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SendtoHand(c,nil,REASON_EFFECT)
 end
-function c101003000.tgfilter(c,tp)
-	return c:GetColumnGroup():IsExists(c101003000.gyfilter,nil,tp)
+function c40516623.tgfilter(c,tp)
+	return c:GetColumnGroup():IsExists(c40516623.gyfilter,nil,tp)
 end
-function c101003000.gyfilter(c,tp)
+function c40516623.gyfilter(c,tp)
 	return c:IsControler(tp) and c:IsLocation(LOCATION_PZONE)
 end
-function c101003000.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101003000.tgfilter,tp,0,LOCATION_MZONE,1,nil,tp) end
+function c40516623.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c40516623.tgfilter,tp,0,LOCATION_MZONE,1,nil,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,1-tp,LOCATION_MZONE)
 end
-function c101003000.gyop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c101003000.tgfilter,tp,0,LOCATION_MZONE,nil,tp)
+function c40516623.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c40516623.tgfilter,tp,0,LOCATION_MZONE,nil,tp)
 	Duel.SendtoGrave(g,REASON_EFFECT)
 end
--- script/c77511331.lua
+++ script/c77511331.lua
@@ -1,6 +1,6 @@
 --Kuro-Obi Karate Spirit
 --Scripted by Eerie Code
-function c101003081.initial_effect(c)
+function c77511331.initial_effect(c)
 	--pendulum summon
 	aux.EnablePendulumAttribute(c)
 	--spirit return
@@ -11,42 +11,42 @@ function c101003081.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCondition(c101003081.thcon)
-	e1:SetTarget(c101003081.thtg)
-	e1:SetOperation(c101003081.thop)
+	e1:SetCondition(c77511331.thcon)
+	e1:SetTarget(c77511331.thtg)
+	e1:SetOperation(c77511331.thop)
 	c:RegisterEffect(e1)
 	--gy
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetTarget(c101003081.gytg)
-	e2:SetOperation(c101003081.gyop)
+	e2:SetTarget(c77511331.gytg)
+	e2:SetOperation(c77511331.gyop)
 	c:RegisterEffect(e2)
 end
-function c101003081.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c77511331.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(Card.IsSummonType,1,nil,SUMMON_TYPE_PENDULUM)
 end
-function c101003081.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c77511331.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
-function c101003081.thop(e,tp,eg,ep,ev,re,r,rp)
+function c77511331.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SendtoHand(c,nil,REASON_EFFECT)
 end
-function c101003081.tgfilter(c,tp)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:GetColumnGroup():IsExists(c101003081.gyfilter,nil,tp)
+function c77511331.tgfilter(c,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:GetColumnGroup():IsExists(c77511331.gyfilter,nil,tp)
 end
-function c101003081.gyfilter(c,tp)
+function c77511331.gyfilter(c,tp)
 	return c:IsControler(tp) and c:IsLocation(LOCATION_PZONE)
 end
-function c101003081.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101003081.tgfilter,tp,0,LOCATION_ONFIELD,1,nil,tp) end
+function c77511331.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c77511331.tgfilter,tp,0,LOCATION_ONFIELD,1,nil,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,1-tp,LOCATION_ONFIELD)
 end
-function c101003081.gyop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c101003081.tgfilter,tp,0,LOCATION_ONFIELD,nil,tp)
+function c77511331.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c77511331.tgfilter,tp,0,LOCATION_ONFIELD,nil,tp)
 	Duel.SendtoGrave(g,REASON_EFFECT)
 end
