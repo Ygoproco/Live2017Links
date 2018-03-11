8f57fd01c7315b6171629d4ff777ac40742cf596
Larry126 <larry126@users.noreply.github.com>
2018-03-11 15:28:49 +0000

    Ojama Token Alias + Material Check fix

---
+++ script/c59123194.lua
@@ -0,0 +1,68 @@
+--覚醒の魔導剣士
+function c59123194.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,1,1,aux.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,59123194)
+	e1:SetCondition(c59123194.thcon)
+	e1:SetTarget(c59123194.thtg)
+	e1:SetOperation(c59123194.thop)
+	c:RegisterEffect(e1)
+	--damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
+	e2:SetCondition(aux.bdocon)
+	e2:SetTarget(c59123194.damtg)
+	e2:SetOperation(c59123194.damop)
+	c:RegisterEffect(e2)
+end
+function c59123194.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:GetMaterial():IsExists(c59123194.pmfilter,1,nil,c)
+end
+function c59123194.thfilter(c)
+	return c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+end
+function c59123194.pmfilter(c,sc)
+	return c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM,sc,SUMMON_TYPE_SYNCHRO)
+end
+function c59123194.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c59123194.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c59123194.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c59123194.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c59123194.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
+function c59123194.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local bc=e:GetHandler():GetBattleTarget()
+	local dam=bc:GetTextAttack()
+	if chk==0 then return dam>0 end
+	Duel.SetTargetCard(bc)
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(dam)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+end
+function c59123194.damop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+		local dam=tc:GetTextAttack()
+		if dam<0 then dam=0 end
+		Duel.Damage(p,dam,REASON_EFFECT)
+	end
+end
