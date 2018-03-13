709f354f71a952cdadc253efab9f4c0a6e114390
Larry126 <larry126@users.noreply.github.com>
2018-03-13 23:20:57 +0000

    Ojama Token Alias + Material Check fix (#476)
    Ojama Token Alias + Material Check fix

---
+++ script/c82734805.lua
@@ -0,0 +1,90 @@
+--インフェルノイド・ティエラ
+function c82734805.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddFusionProcMixRep(c,true,true,aux.FilterBoolFunction(Card.IsFusionSetCard,0xbb),1,99,14799437,23440231)
+	--spsummon success
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(82734805,0))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(c82734805.con)
+	e2:SetTarget(c82734805.tg)
+	e2:SetOperation(c82734805.op)
+	c:RegisterEffect(e2)
+	--material check
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_MATERIAL_CHECK)
+	e3:SetValue(c82734805.valcheck)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+end
+c82734805.material_setcode=0xbb
+function c82734805.valcheck(e,c)
+	local ct=e:GetHandler():GetMaterial():GetClassCount(Card.GetFusionCode)
+	e:GetLabelObject():SetLabel(ct)
+end
+function c82734805.con(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
+end
+function c82734805.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=e:GetLabel()
+	local con3,con5,con8,con10=nil
+	if ct>=3 then
+		con3=Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_EXTRA,0,3,nil)
+			and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_EXTRA,3,nil)
+	end
+	if ct>=5 then
+		con5=Duel.IsPlayerCanDiscardDeck(tp,3) and Duel.IsPlayerCanDiscardDeck(1-tp,3)
+	end
+	if ct>=8 then
+		con8=Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_REMOVED,0,1,nil)
+			and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_REMOVED,1,nil)
+	end
+	if ct>=10 then
+		con10=Duel.GetFieldGroupCount(tp,LOCATION_HAND,LOCATION_HAND)>0
+	end
+	if chk==0 then return con3 or con5 or con8 or con10 end
+end
+function c82734805.op(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetLabel()
+	if ct>=3 then
+		local g1=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_EXTRA,0,nil)
+		local sg1=nil
+		if g1:GetCount()>=3 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+			sg1=g1:Select(tp,3,3,nil)
+		else sg1=g1 end
+		local g2=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,0,LOCATION_EXTRA,nil)
+		local sg2=nil
+		if g2:GetCount()>=3 then
+			Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_TOGRAVE)
+			sg2=g2:Select(1-tp,3,3,nil)
+		else sg2=g2 end
+		sg1:Merge(sg2)
+		if sg1:GetCount()>0 then
+			Duel.SendtoGrave(sg1,REASON_EFFECT)
+		end
+	end
+	if ct>=5 then
+		Duel.BreakEffect()
+		Duel.DiscardDeck(tp,3,REASON_EFFECT)
+		Duel.DiscardDeck(1-tp,3,REASON_EFFECT)
+	end
+	if ct>=8 then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g1=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_REMOVED,0,1,3,nil)
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_TOGRAVE)
+		local g2=Duel.SelectMatchingCard(1-tp,Card.IsFaceup,1-tp,LOCATION_REMOVED,0,1,3,nil)
+		g1:Merge(g2)
+		if g1:GetCount()>0 then
+			Duel.SendtoGrave(g1,REASON_EFFECT+REASON_RETURN)
+		end
+	end
+	if ct>=10 then
+		Duel.BreakEffect()
+		local g1=Duel.GetFieldGroup(tp,LOCATION_HAND,LOCATION_HAND)
+		Duel.SendtoGrave(g1,REASON_EFFECT)
+	end
+end
