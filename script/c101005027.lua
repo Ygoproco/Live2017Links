b1acdc626ac374bfbb018713b92e014ced18f674
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-03-13 21:11:02 -0300

    Tooltip addition for Ritual monster
    Updated RegisterFlagEffect

--- script/c101005027.lua
+++ script/c101005027.lua
@@ -69,7 +69,8 @@ function c101005027.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCondition(c101005027.actcon)
 		e1:SetOperation(c101005027.actop)
 		e1:SetReset(RESET_EVENT+0x1ff0000)
-		tc:RegisterEffect(e1)
+		Duel.RegisterEffect(e1,tp)
+		tc:RegisterFlagEffect(0,RESET_EVENT+0x1fe0000,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(101005027,2))
 	end
 end
 function c101005027.actcon(e,tp,eg,ep,ev,re,r,rp)
