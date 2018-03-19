84a442cf3b6a2d9f197ea07ace7ef92d1b6a31e9
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-03-19 16:28:19 -0300

    small fix for Paladin of Felgrand (#522)
    ReleaseGroupCost was missing treatment for exception group

--- script/proc_workaround.lua
+++ script/proc_workaround.lua
@@ -148,61 +148,69 @@ function Auxiliary.ReleaseCheckTarget(sg,tp,exg,dg)
 	return dg:IsExists(aux.TRUE,1,sg)
 end
 function Auxiliary.RelCheckRecursive(c,tp,sg,mg,exg,mustg,ct,minc,specialchk,...)
-    sg:AddCard(c)
-    ct=ct+1
-    local res=Auxiliary.RelCheckGoal(tp,sg,exg,mustg,ct,minc,specialchk,table.unpack({...})) 
-        or (ct<minc and mg:IsExists(Auxiliary.RelCheckRecursive,1,sg,tp,sg,mg,exg,mustg,ct,minc,specialchk,table.unpack({...})))
-    sg:RemoveCard(c)
-    ct=ct-1
-    return res
+	sg:AddCard(c)
+	ct=ct+1
+	local res=Auxiliary.RelCheckGoal(tp,sg,exg,mustg,ct,minc,specialchk,table.unpack({...})) 
+		or (ct<minc and mg:IsExists(Auxiliary.RelCheckRecursive,1,sg,tp,sg,mg,exg,mustg,ct,minc,specialchk,table.unpack({...})))
+	sg:RemoveCard(c)
+	ct=ct-1
+	return res
 end
 function Auxiliary.RelCheckGoal(tp,sg,exg,mustg,ct,minc,specialchk,...)
-    return ct>=minc and (not specialchk or specialchk(sg,tp,exg,table.unpack({...}))) and sg:Includes(mustg)
+	return ct>=minc and (not specialchk or specialchk(sg,tp,exg,table.unpack({...}))) and sg:Includes(mustg)
 end
 function Duel.CheckReleaseGroupCost(tp,f,ct,use_hand,specialchk,ex,...)
-    local params={...}
+	local params={...}
 	if not ex then ex=Group.CreateGroup() end
-    if not specialchk then specialchk=Auxiliary.ReleaseCheckSingleUse else specialchk=Auxiliary.AND(specialchk,Auxiliary.ReleaseCheckSingleUse) end
-    local g=Duel.GetReleaseGroup(tp,use_hand)
-	if f then g=g:Filter(f,ex,table.unpack(params)) end
-    local exg=Duel.GetMatchingGroup(Auxiliary.ReleaseCostFilter,tp,0,LOCATION_MZONE,g+ex,f,table.unpack(params))
-    local mustg=g:Filter(function(c,tp)return c:IsHasEffect(EFFECT_EXTRA_RELEASE) and c:IsControler(1-tp)end,nil,tp)
-    local mg=g+exg
-    local sg=Group.CreateGroup()
-    return mg:Includes(mustg) and mg:IsExists(Auxiliary.RelCheckRecursive,1,nil,tp,sg,mg,exg,mustg,0,ct,specialchk,table.unpack({...}))
+	if not specialchk then specialchk=Auxiliary.ReleaseCheckSingleUse else specialchk=Auxiliary.AND(specialchk,Auxiliary.ReleaseCheckSingleUse) end
+	local g=Duel.GetReleaseGroup(tp,use_hand)
+	if f then
+		g=g:Filter(f,ex,table.unpack(params))
+	else
+		g=g-ex
+	end
+	local exg=Duel.GetMatchingGroup(Auxiliary.ReleaseCostFilter,tp,0,LOCATION_MZONE,g+ex,f,table.unpack(params))
+	local mustg=g:Filter(function(c,tp)return c:IsHasEffect(EFFECT_EXTRA_RELEASE) and c:IsControler(1-tp)end,nil,tp)
+	local mg=g+exg
+	local sg=Group.CreateGroup()
+	return mg:Includes(mustg) and mg:IsExists(Auxiliary.RelCheckRecursive,1,nil,tp,sg,mg,exg,mustg,0,ct,specialchk,table.unpack({...}))
 end
 function Duel.SelectReleaseGroupCost(tp,f,minc,maxc,use_hand,specialchk,ex,...)
-    local params={...}
+	local params={...}
 	if not ex then ex=Group.CreateGroup() end
-    if not specialchk then specialchk=Auxiliary.ReleaseCheckSingleUse else specialchk=Auxiliary.AND(specialchk,Auxiliary.ReleaseCheckSingleUse) end
-    local g=Duel.GetReleaseGroup(tp,use_hand)
-	if f then g=g:Filter(f,ex,table.unpack(params)) end
-    local exg=Duel.GetMatchingGroup(Auxiliary.ReleaseCostFilter,tp,0,LOCATION_MZONE,g+ex,f,table.unpack(params))
-    local mg=g+exg
-    local mustg=g:Filter(function(c,tp)return c:IsHasEffect(EFFECT_EXTRA_RELEASE) and c:IsControler(1-tp)end,nil,tp)
-    local sg=Group.CreateGroup()
-    local cancel=false
-    sg:Merge(mustg)
-    while #sg<maxc do
-        local cg=mg:Filter(Auxiliary.RelCheckRecursive,sg,tp,sg,mg,exg,mustg,#sg,minc,specialchk,table.unpack({...}))
-        if #cg==0 then break end
-        cancel=#sg>=minc and #sg<=maxc and Auxiliary.RelCheckGoal(tp,sg,exg,mustg,#sg,minc,specialchk,table.unpack({...}))
-        local tc=Group.SelectUnselect(cg,sg,tp,cancel,cancel,1,1)
-        if not tc then break end
-        if #mustg==0 or not mustg:IsContains(tc) then
-            if not sg:IsContains(tc) then
-                sg=sg+tc
-            else
-                sg=sg-tc
-            end
-        end
-    end
-    if #sg==0 then return sg end
-    if #sg~=#(sg-exg) then
-        --LoD is reset for the rest of the turn
-        local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
-        Duel.Hint(HINT_CARD,0,fc:GetCode())
-        fc:RegisterFlagEffect(59160188,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,0)
-    end
+	if not specialchk then specialchk=Auxiliary.ReleaseCheckSingleUse else specialchk=Auxiliary.AND(specialchk,Auxiliary.ReleaseCheckSingleUse) end
+	local g=Duel.GetReleaseGroup(tp,use_hand)
+	if f then
+		g=g:Filter(f,ex,table.unpack(params))
+	else
+		g=g-ex
+	end
+	local exg=Duel.GetMatchingGroup(Auxiliary.ReleaseCostFilter,tp,0,LOCATION_MZONE,g+ex,f,table.unpack(params))
+	local mg=g+exg
+	local mustg=g:Filter(function(c,tp)return c:IsHasEffect(EFFECT_EXTRA_RELEASE) and c:IsControler(1-tp)end,nil,tp)
+	local sg=Group.CreateGroup()
+	local cancel=false
+	sg:Merge(mustg)
+	while #sg<maxc do
+		local cg=mg:Filter(Auxiliary.RelCheckRecursive,sg,tp,sg,mg,exg,mustg,#sg,minc,specialchk,table.unpack({...}))
+		if #cg==0 then break end
+		cancel=#sg>=minc and #sg<=maxc and Auxiliary.RelCheckGoal(tp,sg,exg,mustg,#sg,minc,specialchk,table.unpack({...}))
+		local tc=Group.SelectUnselect(cg,sg,tp,cancel,cancel,1,1)
+		if not tc then break end
+		if #mustg==0 or not mustg:IsContains(tc) then
+			if not sg:IsContains(tc) then
+				sg=sg+tc
+			else
+				sg=sg-tc
+			end
+		end
+	end
+	if #sg==0 then return sg end
+	if #sg~=#(sg-exg) then
+		--LoD is reset for the rest of the turn
+		local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+		Duel.Hint(HINT_CARD,0,fc:GetCode())
+		fc:RegisterFlagEffect(59160188,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,0)
+	end
 	return sg
 end
