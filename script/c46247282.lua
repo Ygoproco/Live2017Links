c04b9bf8445d5099a81d0a94354a664b55299450
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-03-24 14:43:02 -0300

    fix for Tiamaton the Steel Battalion Dragon
    zone checking

--- script/c46247282.lua
+++ script/c46247282.lua
@@ -33,7 +33,7 @@ function c46247282.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCode(EFFECT_DISABLE_FIELD)
-	e4:SetValue(c46247282.disop)
+	e4:SetOperation(c46247282.disop)
 	c:RegisterEffect(e4)
 end
 function c46247282.cfilter(c)
@@ -72,17 +72,16 @@ function c46247282.desop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c46247282.disop(e)
+function c46247282.disop(e,tp)
 	local c=e:GetHandler()
 	local seq=c:GetSequence()
 	local nseq=4-seq
-	if c:GetControler()==1 then seq,nseq=nseq,seq end
 	local flag=0
-	if Duel.CheckLocation(0,LOCATION_MZONE,seq) then flag=flag+(2^seq) end
-	if Duel.CheckLocation(0,LOCATION_SZONE,seq) then flag=flag+((2^seq)<<8) end
-	if Duel.CheckLocation(1,LOCATION_MZONE,nseq) then flag=flag+((2^nseq)<<16) end
-	if Duel.CheckLocation(1,LOCATION_SZONE,nseq) then flag=flag+((2^nseq)<<24) end
-	if seq==1 then flag=flag+(2^5) end
-	if seq==3 then flag=flag+(2^6) end
+	if Duel.CheckLocation(tp,LOCATION_MZONE,seq) then flag=flag+(2^seq) end
+	if Duel.CheckLocation(tp,LOCATION_SZONE,seq) then flag=flag+((2^seq)<<8) end
+	if Duel.CheckLocation(1-tp,LOCATION_MZONE,nseq) then flag=flag+((2^nseq)<<16) end
+	if Duel.CheckLocation(1-tp,LOCATION_SZONE,nseq) then flag=flag+((2^nseq)<<24) end
+	if seq==1 and (Duel.CheckLocation(tp,LOCATION_MZONE,5) and Duel.CheckLocation(1-tp,LOCATION_MZONE,6)) then flag=flag+(2^5) end
+	if seq==3 and (Duel.CheckLocation(tp,LOCATION_MZONE,6) and Duel.CheckLocation(1-tp,LOCATION_MZONE,5)) then flag=flag+(2^6) end
 	return flag
 end
