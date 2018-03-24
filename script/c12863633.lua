e0fd9acae1a7f628aa061b2cb8dceeac1cd522c6
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-03-24 15:27:20 -0300

    small atempt of fix for Curse of the Circle (#532)
    reworked tribute part

---
+++ script/c12863633.lua
@@ -0,0 +1,45 @@
+--五稜星の呪縛
+function c12863633.initial_effect(c)
+	--remove
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,0x1c0)
+	e1:SetTarget(c12863633.target)
+	e1:SetOperation(c12863633.operation)
+	c:RegisterEffect(e1)
+end
+function c12863633.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,nil,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function c12863633.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetProperty(EFFECT_FLAG_ABSOLUTE_TARGET+EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_RELEASE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetAbsoluteRange(tp,0,1)
+		e1:SetTarget(c12863633.rellimit)
+		tc:RegisterEffect(e1)
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+		e3:SetReset(RESET_EVENT+0x1fe0000)
+		e3:SetLabel(1-tp)
+		e3:SetValue(c12863633.synlimit)
+		tc:RegisterEffect(e3)
+	end
+end
+function c12863633.rellimit(e,c,tp,sumtp)
+	return c==e:GetHandler()
+end
+function c12863633.synlimit(e,c)
+	return c and c:IsControler(e:GetLabel())
+end
