01532d3ac760a724e441a99f1194007ea7d85533
edo9300 <edoardo762@gmail.com>
2018-04-17 19:59:41 +0200

    Script cleanup Orgoth the Relentless

--- script/c100227001.lua
+++ script/c100227001.lua
@@ -18,29 +18,22 @@ function c100227001.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100227001.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local d1,d2,d3=Duel.TossDice(tp,3)
-	local atk=(d1+d2+d3)*100
+	local res,atk={0,0,0,0,0,0,false,false,false,false},0
+	for _,i in ipairs({Duel.TossDice(tp,3)}) do
+		atk=atk+(i*100)
+		res[i]=res[i]+1
+		if res[i]>=2 then
+			res[(i+1)//2+6]=true
+		end
+		res[10]=res[i]==3
+	end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(atk)
 	e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END,2)
 	c:RegisterEffect(e1)
-	local res12=false
-	local res34=false
-	local res56=false
-	if d1==d2 and d2==d3 then
-		res12=true
-		res34=true
-		res56=true
-	elseif (d1==d2 and (d1==1 or d1==2)) or (d1==d3 and (d1==1 or d1==2)) or (d2==d3 and (d2==1 or d2==2)) then
-		res12=true
-	elseif (d1==d2 and (d1==3 or d1==4)) or (d1==d3 and (d1==3 or d1==4)) or (d2==d3 and (d2==3 or d2==4)) then
-		res34=true
-	elseif (d1==d2 and (d1==5 or d1==6)) or (d1==d3 and (d1==5 or d1==6)) or (d2==d3 and (d2==5 or d2==6)) then
-		res56=true
-	end
-	if res12 then
+	if res[1+6] or res[10] then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
@@ -51,10 +44,10 @@ function c100227001.operation(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 		c:RegisterEffect(e2)
 	end
-	if res34 then
+	if res[2+6] or res[10] then
 		Duel.Draw(tp,2,REASON_EFFECT)
 	end
-	if res56 then
+	if res[3+6] or res[10] then
 		local e3=Effect.CreateEffect(c)
 		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e3:SetType(EFFECT_TYPE_SINGLE)
