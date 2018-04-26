cba877e17c54c507be3414899dff97b2fcefb0c4
Larry126 <larry126@users.noreply.github.com>
2018-04-26 12:08:16 +0100

    Gabrion clean up
    Fixed timing issue

--- script/c100227024.lua
+++ script/c100227024.lua
@@ -73,14 +73,18 @@ function c100227024.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,g:GetCount())
 end
+function c100227024.cfilter(c,p)
+	return c:IsLocation(LOCATION_DECK) and c:IsControler(p)
+end
 function c100227024.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,nil)
-	if g:GetCount()>0 and Duel.SendtoDeck(g,nil,0,REASON_EFFECT)>0 then
-		local tg=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_DECK)
-		if tg:IsExists(Card.IsControler,1,nil,1-tp) then Duel.ShuffleDeck(1-tp) end
-		if tg:IsExists(Card.IsControler,1,nil,tp) then Duel.ShuffleDeck(tp) end
-		Duel.BreakEffect()
-		Duel.Draw(1-tp,tg:FilterCount(Card.IsControler,nil,1-tp),REASON_EFFECT)
+	if g:GetCount()>0 and Duel.SendtoDeck(g,nil,2,REASON_EFFECT)>0 then
+		local ct=Duel.GetOperatedGroup():FilterCount(c100227024.cfilter,nil,1-tp)
+		if ct>0 then
+			Duel.ShuffleDeck(1-tp)
+			Duel.BreakEffect()
+			Duel.Draw(1-tp,ct,REASON_EFFECT)
+		end
 	end
 end
 function c100227024.rtdcon(e,tp,eg,ep,ev,re,r,rp)
