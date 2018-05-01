abbcd144025c4382352b28141f0a74c751264ee8
edo9300 <edoardo762@gmail.com>
2018-05-01 23:16:22 +0200

    Fixed Revendread Evolution
    You couldn't use cards such as Ritual Raven as tributes for the summon

--- script/c7986397.lua
+++ script/c7986397.lua
@@ -13,27 +13,28 @@ function c7986397.initial_effect(c)
 	if not AshBlossomTable then AshBlossomTable={} end
 	table.insert(AshBlossomTable,e1)
 end
-function c7986397.filter(c,e,tp,m)
+function c7986397.filter(c,e,tp,m1,m2)
 	if not c:IsSetCard(0x106) or bit.band(c:GetType(),0x81)~=0x81
 		or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
-	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
+	local lv=c:GetLevel()
+	local mg=m1:Filter(Card.IsCanBeRitualMaterial,c,c)
+	local mg2=m2:Filter(Card.IsCanBeRitualMaterial,c,c)
 	if c.mat_filter then
 		mg=mg:Filter(c.mat_filter,nil)
+		mg2=mg2:Filter(c.mat_filter,nil)
 	end
-	local sg=Group.CreateGroup()
-	return mg:IsExists(c7986397.checkRecursive,1,nil,c,tp,sg,mg)
+	return mg:CheckWithSumEqual(Card.GetRitualLevel,lv,1,99999,c) or mg2:IsExists(function(c,spc)Duel.SetSelectedCard(c)return mg:CheckWithSumEqual(Card.GetRitualLevel,lv,0,99999,spc)end,nil,1,c)
 end
-function c7986397.checkRecursive(c,rc,tp,sg,mg)
-	sg:AddCard(c)
-	local res=sg:GetSum(Card.GetRitualLevel,rc)<=rc:GetLevel() 
-		and (c7986397.checkGoal(tp,sg,rc) or mg:IsExists(c7986397.checkRecursive,1,sg,rc,tp,sg,mg))
-	sg:RemoveCard(c)
+function c7986397.checkvalid(c,rc,tp,sg,mg,mg2)
+	local deck = (mg2-sg)<mg2
+	if mg2:IsContains(c) and deck then return false end
+	Duel.SetSelectedCard(sg+c)
+	local res=mg:CheckWithSumEqual(Card.GetRitualLevel,rc:GetLevel(),0,99999,rc)
+	if not res and not deck then
+		Duel.SetSelectedCard(sg+c)
+		res=(mg+sg):CheckWithSumEqual(Card.GetRitualLevel,rc:GetLevel(),0,99999,rc)
+	end
 	return res
-end
-function c7986397.checkGoal(tp,sg,rc)
-	return sg:GetSum(Card.GetRitualLevel,rc)==rc:GetLevel() --sg:CheckWithSumEqual(Card.GetRitualLevel,rc:GetLevel(),1,rc:GetLevel(),rc)
-		and sg:FilterCount(Card.IsLocation,nil,LOCATION_DECK)<=1
-		and aux.ReleaseCheckMMZ(sg,tp)
 end
 function c7986397.exfilter0(c)
 	return c:IsSetCard(0x106) and c:IsLevelAbove(1) and c:IsAbleToGrave()
@@ -41,31 +42,32 @@ end
 function c7986397.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local mg=Duel.GetRitualMaterial(tp)
-		local sg=Duel.GetMatchingGroup(c7986397.exfilter0,tp,LOCATION_DECK,0,nil)
-		mg:Merge(sg)
+		local sg=Duel.GetMatchingGroup(c7986397.exfilter0,tp,LOCATION_DECK,0,nil)-mg
 		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		return ft>-1 and Duel.IsExistingMatchingCard(c7986397.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp,mg)
+		return ft>-1 and Duel.IsExistingMatchingCard(c7986397.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp,mg,sg)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
 function c7986397.activate(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetRitualMaterial(tp)
-	local sg=Duel.GetMatchingGroup(c7986397.exfilter0,tp,LOCATION_DECK,0,nil)
-	mg:Merge(sg)
+	local sg=Duel.GetMatchingGroup(c7986397.exfilter0,tp,LOCATION_DECK,0,nil)-mg
+	local full=mg+sg
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c7986397.filter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp,mg,ft)
+	local tg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c7986397.filter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp,mg,sg,ft)
 	local rc=tg:GetFirst()
 	if rc then
-		mg=mg:Filter(Card.IsCanBeRitualMaterial,rc,rc)
+	local lv=rc:GetLevel()
+		full=full:Filter(Card.IsCanBeRitualMaterial,rc,rc)
 		if rc.mat_filter then
-			mg=mg:Filter(rc.mat_filter,nil)
+			full=full:Filter(rc.mat_filter,nil)
 		end
 		local mat=Group.CreateGroup()
 		while true do
-			local cg=mg:Filter(c7986397.checkRecursive,mat,rc,tp,mat,mg)
+			local cg=full:Filter(c7986397.checkvalid,mat,rc,tp,mat,mg,sg)
 			if #cg==0 then break end
-			local tc=Group.SelectUnselect(cg,mat,tp,c7986397.checkGoal(tp,mat,rc),false,1,1)
+			local cancelable=(function()Duel.SetSelectedCard(mat)return Group.CreateGroup():CheckWithSumEqual(Card.GetRitualLevel,lv,0,0,rc)end)()
+			local tc=cg:SelectUnselect(mat,tp,cancelable,cancelable,1,1)
 			if not tc then break end
 			if not mat:IsContains(tc) then
 				mat=mat+tc
