53004d05c266a62285e1557efab06f956c350183
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-05-01 00:08:19 -0300

    small fix for World Legacy - "World Crown"
    was abble to special summon in extra monster zone

--- script/c27918365.lua
+++ script/c27918365.lua
@@ -43,11 +43,11 @@ end
 function c27918365.spcon(e,c)
 	if c==nil then return true end
 	local tp=e:GetHandlerPlayer()
-	local zone=Duel.GetLinkedZone(tp)
+	local zone=Duel.GetLinkedZone(tp)&0x1f
 	return zone~=0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp,zone)
 end
 function c27918365.spval(e,c)
-	return 0,Duel.GetLinkedZone(c:GetControler())
+	return 0,Duel.GetLinkedZone(c:GetControler())&0x1f
 end
 function c27918365.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and re:IsActiveType(TYPE_MONSTER) and re:GetHandler():GetSummonLocation()==LOCATION_EXTRA
