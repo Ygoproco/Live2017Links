e7be14782f65da282c5099ad8261ba433c524ff8
EerieCode <eeriecode@gmail.com>
2018-05-08 21:51:35 +0200

    Added new cards
    Darkness Metal, the Dark Steel Dragon
    Emergency Cyber
    Hibernation Dragon
    Triggering Wurm
    Dark Armed, the Annihilation Dragon

---
+++ script/c100227042.lua
@@ -0,0 +1,68 @@
+--トリガー・ヴルム 
+--Triggering Wurm
+function c100227042.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100227042,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCode(EVENT_BE_MATERIAL)
+	e1:SetCountLimit(1,100227042)
+	e1:SetCondition(c100227042.spcon)
+	e1:SetTarget(c100227042.sptg)
+	e1:SetOperation(c100227042.spop)
+	c:RegisterEffect(e1)
+	--draw
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100227042,1))
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCountLimit(1,100227142+100)
+	e2:SetCondition(c100227042.drcon)
+	e2:SetTarget(c100227042.drtg)
+	e2:SetOperation(c100227042.drop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e3)	
+end
+function c100227042.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return r==REASON_LINK and re:GetHandler():IsAttribute(ATTRIBUTE_DARK)
+end
+function c100227042.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local zone=re:GetHandler():GetLinkedZone(tp)&0x1f
+	if chk==0 then return zone~=0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK,tp,zone) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_GRAVE)
+end
+function c100227042.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local zone=re:GetHandler():GetLinkedZone(tp)&0x1f
+	if c:IsRelateToEffect(e) and zone~=0 and Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP_ATTACK,zone) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e1,true)
+		Duel.SpecialSummonComplete()
+	end
+end
+function c100227042.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return r&REASON_EFFECT==REASON_EFFECT and re:GetHandler():IsType(TYPE_LINK) and re:IsActivated()
+end
+function c100227042.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c100227042.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
---
+++ script/c100228003.lua
@@ -0,0 +1,62 @@
+--撃滅龍 ダーク・アームド
+--Dark Armed, the Annihilator Dragon
+--Scripted by ahtelel
+function c100228003.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,7,2,c100228003.ovfilter,aux.Stringid(100228003,0),99,c100228003.xyzop)
+	c:EnableReviveLimit()
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE)
+	e1:SetDescription(aux.Stringid(100228003,1))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,100228003)
+	e1:SetCost(c100228003.cost)
+	e1:SetTarget(c100228003.target)
+	e1:SetOperation(c100228003.operation)
+	c:RegisterEffect(e1,false,1)
+end
+function c100228003.ovfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevelAbove(5) and Duel.GetMatchingGroupCount(Card.IsAttribute,c:GetControler(),LOCATION_GRAVE,0,nil,ATTRIBUTE_DARK)==5
+end
+function c100228003.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,100228003)==0 end
+	Duel.RegisterFlagEffect(tp,100228003,RESET_PHASE+PHASE_END,0,1)
+	return true
+end
+function c100228003.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function c100228003.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c100228003.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		if Duel.Destroy(tc,REASON_EFFECT)~=0 then
+			Duel.BreakEffect()
+			local g
+			if Duel.IsPlayerAffectedByEffect(tp,69832741) then
+				g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,0,1,1,nil)
+			else
+				g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_GRAVE,0,1,1,nil)
+			end
+			if g:GetCount()>0 then
+			Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+			end
+		end
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e:GetHandler():RegisterEffect(e1)
+	end
+end
