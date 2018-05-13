46c97a045c8333ab662b77d0c9591481ae7a583b
senpaizuri3 <senpaizuri3@gmail.com>
2018-05-13 16:47:53 -0400

    Fix "Glife the Phantom Bird"

--- script/c899287.lua
+++ script/c899287.lua
@@ -50,7 +50,7 @@ function c899287.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(1-tp) end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_SZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_SZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,function(c) return c:GetSequence()<5 end,tp,0,LOCATION_SZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c899287.desop(e,tp,eg,ep,ev,re,r,rp)
