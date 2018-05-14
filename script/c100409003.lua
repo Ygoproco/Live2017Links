c76917db7596f39eca4287cd863868514b822aa8
senpaizuri3 <senpaizuri3@gmail.com>
2018-05-14 19:20:12 -0400

    Fixed "Bingo Machine GO! GO!"

--- script/c100409003.lua
+++ script/c100409003.lua
@@ -8,8 +8,6 @@ function c100409003.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,100409003+EFFECT_COUNT_CODE_OATH)
-	e1:SetCondition(c100409003.condition)
-	e1:SetCost(c100409003.cost)
 	e1:SetTarget(c100409003.target)
 	e1:SetOperation(c100409003.operation)
 	c:RegisterEffect(e1)
@@ -22,28 +20,19 @@ end
 function c100409003.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local g=Duel.GetMatchingGroup(c100409003.filter,tp,LOCATION_DECK,0,nil)
-		return g:GetClassCount(Card.GetCode)>=3
+		return g:GetCount()>=3
 	end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
 end
 function c100409003.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(c100409003.filter,tp,LOCATION_DECK,0,nil)
-	if g:GetClassCount(Card.GetCode)>=3 then
+	if g:GetCount()>=3 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-		local sg1=g:Select(tp,1,1,nil)
-		g:Remove(Card.IsCode,nil,sg1:GetFirst():GetCode())
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-		local sg2=g:Select(tp,1,1,nil)
-		g:Remove(Card.IsCode,nil,sg2:GetFirst():GetCode())
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-		local sg3=g:Select(tp,1,1,nil)
-		sg1:Merge(sg2)
-		sg1:Merge(sg3)
-		Duel.ConfirmCards(1-tp,sg1)
+		local sg=g:Select(tp,3,3,nil)
+		Duel.ConfirmCards(1-tp,sg)
 		Duel.ShuffleDeck(tp)
 		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_ATOHAND)
-		local tg=sg1:Select(1-tp,1,1,nil)
-		local tc=tg:GetFirst()
+		local tc=sg:Select(1-tp,1,1,nil):GetFirst()
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
