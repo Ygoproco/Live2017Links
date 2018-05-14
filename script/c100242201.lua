c5b4216c423645d3512dbdb2736d0fba962aa88a
EerieCode <eeriecode@gmail.com>
2018-05-14 07:36:02 +0200

    Added "Manifested Skull Archfiend"

---
+++ script/c100242201.lua
@@ -0,0 +1,57 @@
+--デーモンの降臨
+--Manifested Skull Archfiend
+--Scripted by AlphaKretin
+function c100242201.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddFusionProcMix(c,true,true,70781052,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK))
+	--code
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(70781052)
+	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsCode,70781052))
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCondition(c100242201.spcon)
+	e3:SetTarget(c100242201.sptg)
+	e3:SetOperation(c100242201.spop)
+	c:RegisterEffect(e3)
+end
+function c100242201.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_FUSION)
+		and rp~=tp and c:GetPreviousControler()==tp
+end
+function c100242201.spfilter(c,e,tp)
+	return c:IsCode(70781052) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function c100242201.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
+		return Duel.IsExistingMatchingCard(c100242201.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+end
+function c100242201.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100242201.spfilter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	if tc then 
+		Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)
+	end
+end
