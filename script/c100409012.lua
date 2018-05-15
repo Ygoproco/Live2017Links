a0ab5bd116f6f6e401b04da23cef092c627806a7
EerieCode <eeriecode@gmail.com>
2018-05-15 21:57:47 +0200

    Added new cards
    Cyber Eternity Dragon
    Cyber Pharos
    Cyberload Fusion

---
+++ script/c100409012.lua
@@ -0,0 +1,95 @@
+--サイバー・エタニティ・ドラゴン
+-- Cyber Eternity Dragon
+function c100409012.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcMixN(c,true,true,aux.FilterBoolFunction(Card.IsFusionSetCard,0x1093),1,aux.FilterBoolFunction(Card.IsRace,RACE_MACHINE),2)
+	--cannot be target
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetCondition(c100409012.imunecond)
+	e0:SetValue(aux.tgoval)
+	c:RegisterEffect(e0)
+	--cannot be destroyed
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c100409012.imunecond)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	--summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100409012,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCondition(c100409012.spcon)
+	e2:SetTarget(c100409012.sptg)
+	e2:SetOperation(c100409012.spop)
+	c:RegisterEffect(e2)
+	--protection
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100409012,1))
+	e3:SetCategory(CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,100409012)
+	e3:SetCost(aux.bfgcost)
+	e3:SetOperation(c100409012.operation)
+	c:RegisterEffect(e3)
+end
+c100409012.material_setcode={0x93,0x1093}
+function c100409012.imunecond(e)
+	return Duel.IsExistingMatchingCard(c100409012.filter,e:GetHandlerPlayer(),LOCATION_GRAVE,0,1,nil)
+end
+function c100409012.filter(c)
+	return c:IsType(TYPE_FUSION)
+end
+function c100409012.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_FUSION) and rp~=tp
+end
+function c100409012.spfilter(c,e,tp)
+	return c:IsCode(70095154) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100409012.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100409012.spfilter,tp,0x13,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0x13)
+end
+function c100409012.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100409012.spfilter),tp,0x13,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
+	end
+end
+
+function c100409012.operation(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetTarget(c100409012.tg)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetValue(c100409012.tg)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetValue(aux.tgoval)
+	Duel.RegisterEffect(e2,tp)
+end
+function c100409012.tgvalue(e,re,rp)
+	return rp~=e:GetHandlerPlayer()
+end
+function c100409012.tg(e,c)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION)
+end
