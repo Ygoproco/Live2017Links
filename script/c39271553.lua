cb18a678d5653733376129add6bca13c21ac1493
senpaizuri3 <senpaizuri3@gmail.com>
2018-05-17 22:39:12 -0400

    Small fix "F.A. Auto Navigator"

--- script/c39271553.lua
+++ script/c39271553.lua
@@ -47,7 +47,7 @@ function c39271553.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
 		local tc=Duel.GetFirstTarget()
 		if tc:IsRelateToEffect(e) then
-			local lv=tc:GetLevel()-tc:GetOriginalLevel()
+			local lv=math.abs(tc:GetLevel()-tc:GetOriginalLevel())
 			if lv==0 then return end
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
