62f28f433e6bfb4b266caf8b8664f46e8a03284b
Larry126 <larry126@users.noreply.github.com>
2018-05-29 18:40:09 +0100

    Reverted field check

--- script/c73405179.lua
+++ script/c73405179.lua
@@ -36,9 +36,12 @@ end
 function c73405179.thfilter(c,fc)
 	return (c:IsCode(41916534) or (fc and c:IsRace(RACE_WARRIOR))) and c:IsAbleToHand()
 end
+function c73405179.fieldcond(c)
+	return c:IsFaceup() and c:IsCode(72283691)
+end
 function c73405179.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then 
-		local fc=Duel.IsEnvironment(72283691)
+		local fc=Duel.IsExistingMatchingCard(c73405179.fieldcond,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil)
 		return Duel.IsExistingMatchingCard(c73405179.thfilter,tp,LOCATION_DECK,0,1,nil,fc)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
