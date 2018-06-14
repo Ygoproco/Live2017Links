bf8cbb2268cc1c32124f6ef9b21accce2cefbdea
AlphaKretin <deewhydeeecks@gmail.com>
2018-06-14 14:01:25 +0930

    Fix "Cynet" Cards (#834)
    * Fix "Cynet Optimise"
    
    * Fix "Cynet Conflict"
    
    * fix

--- script/c100334023.lua
+++ script/c100334023.lua
@@ -1,7 +1,7 @@
 --サイバネット・オプティマイズ
 --Cynet Optimize
 --Script by AlphaKretin
-function c100334023.intial_effect(c)
+function c100334023.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
--- script/c100334032.lua
+++ script/c100334032.lua
@@ -8,26 +8,26 @@ function c100334032.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetCountLimit(1,100334032+EFFECT_COUNT_CODE_OATH)
-	e1:SetCondition(c100334023.condition)
-	e1:SetTarget(c100334023.target)
-	e1:SetOperation(c100334023.activate)
+	e1:SetCondition(c100334032.condition)
+	e1:SetTarget(c100334032.target)
+	e1:SetOperation(c100334032.activate)
 	c:RegisterEffect(e1)
 end
-function c100334023.cfilter(c)
+function c100334032.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x101)
 end
-function c100334023.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c100334023.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
+function c100334032.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100334032.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
 		and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
 end
-function c100334023.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100334032.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
 	end
 end
-function c100334023.activate(e,tp,eg,ep,ev,re,r,rp)
+function c100334032.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ec=re:GetHandler()
 	if Duel.NegateActivation(ev) and ec:IsRelateToEffect(re) then
 		ec:CancelToGrave()
@@ -37,12 +37,12 @@ function c100334023.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetTargetRange(0,1)
-		e1:SetTarget(c100334023.aclimit)
+		e1:SetTarget(c100334032.aclimit)
 		e1:SetLabel(ec:GetOriginalCode())
 		e1:SetReset(RESET_PHASE+PHASE_END,2)
 		Duel.RegisterEffect(e1,1-tp)
 	end
 end
-function c100334023.aclimit(e,re,tp)
+function c100334032.aclimit(e,re,tp)
 	return re:GetHandler():IsOriginalCode(e:GetLabel()) and not re:GetHandler():IsImmuneToEffect(e)
 end
