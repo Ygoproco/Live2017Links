ecd5916029a6b31bc4aded2e588dc25737ddce9a
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-06-29 05:50:13 -0300

    Fixed "Thunder Dragon" Fusion monsters (#880)
    * Fixed "Thunder Dragon Lord"
    
    Fixed the bug where you could use its effect in the summoning negation window
    
    * Fixed "Superbolt Thunder Dragon"
    
    Fixed the bug where the destruction replacement effect could be used even when he card was face-down.

--- script/c101006037.lua
+++ script/c101006037.lua
@@ -35,6 +35,7 @@ function c101006037.initial_effect(c)
 	--destroy replace
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e4:SetCode(EFFECT_DESTROY_REPLACE)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetTarget(c101006037.desreptg)
