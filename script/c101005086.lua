0a5b5a4c021c269f4a97f0f13d42146e6e4fd78c
EerieCode <eeriecode@gmail.com>
2018-07-03 21:54:55 +0200

    Added new cards
    Salamangreat Heart
    Danger! Bigfoot!
    Danger! Nessie!
    Danger! Chupacabra!
    Danger!? Jackalope?
    Realm of Danger!
    Danger! Zone

---
+++ script/c101005086.lua
@@ -0,0 +1,59 @@
+--Realm of Danger!
+--Scripted by AlphaKretin
+function c101005086.intial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--cannot be target
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(c101005086.target)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+	--direct attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetOperation(c101005086.daop)
+	c:RegisterEffect(e3)
+end
+function c101005086.target(e,c)
+	return c:IsSetCard(0x233) and c:IsStatus(STATUS_SPSUMMON_TURN)
+end
+function c101005086.dafilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x233)
+end
+function c101005086.datg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101005086.dafilter(chkc)
+	if chk==0 return Duel.IsExistingTarget(c101005086.dafilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,0,1,1,c)
+end
+function c101005086.daop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not (c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup()) then return end
+	c:SetCardTarget(tc)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_SINGLE_RANGE))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetConditon(c101005086.rcon)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
+	c:RegisterEffect(e2)
+end
+function c101005086.rcon(e)
+	return e:GetOwner():IsHasCardTarget(e:GetHandler())
+end
