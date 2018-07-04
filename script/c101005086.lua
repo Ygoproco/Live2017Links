436e29da51fca6c3ed9ea0626673c7acbe59a723
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-07-04 20:17:22 -0300

    small fix for danger field(?) (#903)
    wrong filter in target selection

--- script/c101005086.lua
+++ script/c101005086.lua
@@ -36,7 +36,7 @@ function c101005086.datg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101005086.dafilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c101005086.dafilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,0,1,1,c)
+	local g=Duel.SelectTarget(tp,c101005086.dafilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function c101005086.daop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
