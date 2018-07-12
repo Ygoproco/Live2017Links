9797a887186ea033686076b057bf66b44fbca97c
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-07-12 17:58:42 -0300

    Update c101006015.lua
    missing splim

--- script/c101006015.lua
+++ script/c101006015.lua
@@ -28,5 +28,16 @@ function c101006015.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+		e1:SetTargetRange(1,0)
+		e1:SetTarget(c101006015.splimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 	end
+end
+function c101006015.splimit(e,c,sump,sumtype,sumpos,targetp)
+	return not c:IsAttribute(ATTRIBUTE_DARK)
 end
