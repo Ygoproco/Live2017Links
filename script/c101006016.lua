e542eabc2e2af733873b4e75922927bb890e3ec1
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-07-12 22:32:10 -0300

    fix for Orphegel Diver/canon/Scherzone (#942)
    fix for Orphegel Diver/canon/Scherzone

--- script/c101006016.lua
+++ script/c101006016.lua
@@ -18,13 +18,13 @@ function c101006016.filter(c,e,tp)
 end
 function c101006016.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c101006016.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+		and Duel.IsExistingMatchingCard(c101006016.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function c101006016.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c101006016.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c101006016.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -38,5 +38,5 @@ function c101006016.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c101006016.splimit(e,c,sump,sumtype,sumpos,targetp)
-	return not ( c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_DARK) )
+	return not c:IsAttribute(ATTRIBUTE_DARK)
 end
