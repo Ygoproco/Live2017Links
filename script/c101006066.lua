cf82412d8dab92f5727f086cdf3ccc67a8a190a8
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-07-14 01:08:28 -0300

    fix for Parallel Panzer
    fixed 2nd effect

--- script/c101006066.lua
+++ script/c101006066.lua
@@ -5,6 +5,7 @@ function c101006066.initial_effect(c)
 	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsType,TYPE_LINK))
 	--move to a zone it point
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101006066,0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1)
@@ -13,21 +14,14 @@ function c101006066.initial_effect(c)
 	c:RegisterEffect(e1)
 	--destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetDescription(aux.Stringid(101006066,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,101006066)
-	e2:SetCondition(c101006066.dcondition)
+	e2:SetCost(c101006066.dcost)
 	e2:SetTarget(c101006066.dtarget)
 	e2:SetOperation(c101006066.doperation)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_LEAVE_FIELD_P)
-	e3:SetOperation(c101006066.checkop)
-	e3:SetLabelObject(e2)
-	c:RegisterEffect(e3)
 end
 function c101006066.mtarget(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -53,52 +47,31 @@ function c101006066.moperation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.MoveSequence(ec,nseq)
 	end
 end
-function c101006066.dcondition(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return c:GetPreviousLocation()==LOCATION_SZONE and not c:IsReason(REASON_LOST_TARGET)
-end
-function c101006066.checkop(e,tp)
+function c101006066.dcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local ec=c:GetEquipTarget()
-	if ec then
-		e:GetLabelObject():SetLabel(ec:GetControler()|(ec:GetSequence()<<2))
-	else
-		e:GetLabelObject():SetLabel(-1)
-	end
+	if chk==0 then return ec and c:IsAbleToGraveAsCost() end
+	Duel.SetTargetCard(ec)
+	Duel.SendtoGrave(c,REASON_COST)
 end
-function c101006066.filter(c,tp,seq)
-	local cseq=c:GetSequence()
-	local cp=c:GetControler()
-	if c:IsLocation(LOCATION_MZONE) then
-		if seq==1 or seq==5 then
-			return (cp==tp and (cseq==1 or cseq==5)) or (cp~=tp and (cseq==3 or cseq==6))
-		elseif seq==3 or seq==6 then
-			return (cp==tp and (cseq==3 or cseq==6)) or (cp~=tp and (cseq==1 or cseq==5))
-		else
-			return (cp==tp and cseq==seq) or (cp~=tp and cseq==4-seq)
-		end
-	else
-		if seq==5 or seq==6 then
-			return (cp==tp and ((cseq==1 and seq==5) or (cseq==3 and seq==6)))
-				or (cp~=tp and ((cseq==1 and seq==6) or (cseq==3 and seq==5)))
-		else
-			return (cp==tp and cseq==seq) or (cp~=tp and cseq==4-seq)
-		end
-	end
+function c101006066.filter(c,g,tc)
+	return c==tc or g:IsContains(c)
 end
 function c101006066.dtarget(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local cp=e:GetLabel()&0x3
-	local seq=e:GetLabel()>>2
-	local g=Duel.GetMatchingGroup(c101006066.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,cp,seq)
-	if chk==0 then return seq>=0 and #g>0 end
+	local ec=c:GetEquipTarget()
+	if not ec then return end
+	local g=Duel.GetMatchingGroup(c101006066.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,c,ec:GetColumnGroup(),ec)
+	if chk==0 then return #g>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c101006066.doperation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local cp=e:GetLabel()&0x3
-	local seq=e:GetLabel()>>2
+	local tc=Duel.GetFirstTarget()
+	if not tc or not tc:IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local dg=Duel.SelectMatchingCard(tp,c101006066.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,cp,seq)
-	Duel.Destroy(dg,REASON_EFFECT)
+	local dg=Duel.SelectMatchingCard(tp,c101006066.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,tc:GetColumnGroup(),tc)
+	if #dg>0 then
+		Duel.Destroy(dg,REASON_EFFECT)
+	end
 end
