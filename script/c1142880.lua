5df057c144638a2564c678b9d04896d910eb01bb
EerieCode <eeriecode@gmail.com>
2018-07-24 21:46:39 +0200

    Official ID
    Cyber Dragon Nachster

--- script/c1142880.lua
+++ script/c1142880.lua
@@ -1,7 +1,7 @@
 --サイバー・ドラゴン・ネクステア
 --Cyber Dragon Nächster
 --Scripted by ahtelel
-function c100200152.initial_effect(c)
+function c1142880.initial_effect(c)
 	--name change
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -12,61 +12,61 @@ function c100200152.initial_effect(c)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100200152,0))
+	e2:SetDescription(aux.Stringid(1142880,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_HAND)
-	e2:SetCountLimit(1,100200152)
-	e2:SetCost(c100200152.spcost)
-	e2:SetTarget(c100200152.sptg)
-	e2:SetOperation(c100200152.spop)
+	e2:SetCountLimit(1,1142880)
+	e2:SetCost(c1142880.spcost)
+	e2:SetTarget(c1142880.sptg)
+	e2:SetOperation(c1142880.spop)
 	c:RegisterEffect(e2)
 	--special summon (gy)
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100200152,1))
+	e3:SetDescription(aux.Stringid(1142880,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_SUMMON_SUCCESS)
 	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e3:SetCountLimit(1,100200252)
-	e3:SetTarget(c100200152.sptg2)
-	e3:SetOperation(c100200152.spop2)
+	e3:SetCountLimit(1,1142881)
+	e3:SetTarget(c1142880.sptg2)
+	e3:SetOperation(c1142880.spop2)
 	c:RegisterEffect(e3)
 	local e4=e3:Clone()
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e4)
 end
-c100200152.listed_names={CARD_CYBER_DRAGON }
-function c100200152.cfilter(c)
+c1142880.listed_names={CARD_CYBER_DRAGON }
+function c1142880.cfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsDiscardable()
 end
-function c100200152.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100200152.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
-	Duel.DiscardHand(tp,c100200152.cfilter,1,1,REASON_COST+REASON_DISCARD,e:GetHandler())
+function c1142880.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c1142880.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,c1142880.cfilter,1,1,REASON_COST+REASON_DISCARD,e:GetHandler())
 end
-function c100200152.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c1142880.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c100200152.spop(e,tp,eg,ep,ev,re,r,rp)
+function c1142880.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c100200152.spfilter(c,e,tp)
+function c1142880.spfilter(c,e,tp)
 	return c:IsRace(RACE_MACHINE) and (c:GetAttack()==2100 or c:GetDefense()==2100) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
-function c100200152.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100200152.spfilter(chkc,e,tp) end
+function c1142880.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c1142880.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100200152.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c1142880.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100200152.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c1142880.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c100200152.spop2(e,tp,eg,ep,ev,re,r,rp)
+function c1142880.spop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then 
@@ -77,10 +77,10 @@ function c100200152.spop2(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c100200152.splimit)
+	e1:SetTarget(c1142880.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
-function c100200152.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+function c1142880.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsRace(RACE_MACHINE)
 end
