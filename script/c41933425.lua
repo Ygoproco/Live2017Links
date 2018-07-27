0cadc503fc1166aba2a42d793ec3e658977c95b9
EerieCode <eeriecode@gmail.com>
2018-07-27 21:51:15 +0200

    Official IDs and TCG update
    Cybernetic Horizon

--- script/c41933425.lua
+++ script/c41933425.lua
@@ -1,15 +1,15 @@
 --Contact Gate
 --Scripted by Eerie Code
-function c101005000.initial_effect(c)
+function c41933425.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,101005000+EFFECT_COUNT_CODE_OATH)
-	e1:SetCost(c101005000.cost)
-	e1:SetTarget(c101005000.target)
-	e1:SetOperation(c101005000.activate)
+	e1:SetCountLimit(1,41933425+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c41933425.cost)
+	e1:SetTarget(c41933425.target)
+	e1:SetOperation(c41933425.activate)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
@@ -18,75 +18,75 @@ function c101005000.initial_effect(c)
 	e2:SetCode(EVENT_TO_DECK)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCondition(c101005000.spcon)
+	e2:SetCondition(c41933425.spcon)
 	e2:SetCost(aux.bfgcost)
-	e2:SetTarget(c101005000.sptg)
-	e2:SetOperation(c101005000.spop)
+	e2:SetTarget(c41933425.sptg)
+	e2:SetOperation(c41933425.spop)
 	c:RegisterEffect(e2)
 end
-c101005000.listed_names={CARD_NEOS }
-function c101005000.cfilter(c)
+c41933425.listed_names={CARD_NEOS }
+function c41933425.cfilter(c)
 	return c:IsSetCard(0x1f) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
 end
-function c101005000.filter(c,e,tp)
+function c41933425.filter(c,e,tp)
 	return c:IsSetCard(0x1f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c101005000.rescon1(sg,e,tp,mg)
-	local g=Duel.GetMatchingGroup(c101005000.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,sg,e,tp)
+function c41933425.rescon1(sg,e,tp,mg)
+	local g=Duel.GetMatchingGroup(c41933425.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,sg,e,tp)
 	return aux.ChkfMMZ(2)(sg,e,tp,mg) and sg:GetClassCount(Card.GetCode)==2 
-		and aux.SelectUnselectGroup(g,e,tp,2,2,c101005000.rescon2,0)
+		and aux.SelectUnselectGroup(g,e,tp,2,2,c41933425.rescon2,0)
 end
-function c101005000.rescon2(sg,e,tp,mg)
+function c41933425.rescon2(sg,e,tp,mg)
 	return sg:GetClassCount(Card.GetCode)==2
 end
-function c101005000.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local rg=Duel.GetMatchingGroup(c101005000.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,nil)
-	if chk==0 then return aux.SelectUnselectGroup(rg,e,tp,2,2,c101005000.rescon1,0) end
-	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,c101005000.rescon1,1,tp,HINTMSG_REMOVE)
+function c41933425.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local rg=Duel.GetMatchingGroup(c41933425.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(rg,e,tp,2,2,c41933425.rescon1,0) end
+	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,c41933425.rescon1,1,tp,HINTMSG_REMOVE)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c101005000.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c41933425.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
-function c101005000.activate(e,tp,eg,ep,ev,re,r,rp)
+function c41933425.activate(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c101005000.splimit)
+	e1:SetTarget(c41933425.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 or Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
-	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101005000.filter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,nil,e,tp)
-	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,c101005000.rescon2,1,tp,HINTMSG_SPSUMMON)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c41933425.filter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,nil,e,tp)
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,c41933425.rescon2,1,tp,HINTMSG_SPSUMMON)
 	if #sg==2 then
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c101005000.splimit(e,c)
+function c41933425.splimit(e,c)
 	return not c:IsType(TYPE_FUSION) and c:IsLocation(LOCATION_EXTRA)
 end
-function c101005000.spcfilter(c,tp)
+function c41933425.spcfilter(c,tp)
 	return c:IsPreviousPosition(POS_FACEUP) and aux.IsMaterialListCode(c,CARD_NEOS)
 		and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_ONFIELD)
 end
-function c101005000.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c101005000.spcfilter,1,nil,tp)
+function c41933425.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c41933425.spcfilter,1,nil,tp)
 end
-function c101005000.spfilter(c,e,tp)
-	return c101005000.filter(c,e,tp) and c:IsFaceup()
+function c41933425.spfilter(c,e,tp)
+	return c41933425.filter(c,e,tp) and c:IsFaceup()
 end
-function c101005000.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c41933425.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c101005000.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c41933425.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,1,nil,tp,LOCATION_REMOVED)
 end
-function c101005000.spop(e,tp,eg,ep,ev,re,r,rp)
+function c41933425.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c101005000.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c41933425.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
--- script/c43316238.lua
+++ script/c43316238.lua
@@ -1,46 +1,46 @@
 --Danger! Bigfoot!
 --Scripted by AlphaKretin
-function c101005082.initial_effect(c)
+function c43316238.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(101005082,0))
+	e1:SetDescription(aux.Stringid(43316238,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(c101005082.spcost)
-	e1:SetTarget(c101005082.sptg)
-	e1:SetOperation(c101005082.spop)
+	e1:SetCost(c43316238.spcost)
+	e1:SetTarget(c43316238.sptg)
+	e1:SetOperation(c43316238.spop)
 	c:RegisterEffect(e1)
 	--destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101005082,1))
+	e2:SetDescription(aux.Stringid(43316238,1))
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCountLimit(1,101005082)
-	e2:SetCondition(c101005082.descon)
-	e2:SetTarget(c101005082.destg)
-	e2:SetOperation(c101005082.desop)
+	e2:SetCountLimit(1,43316238)
+	e2:SetCondition(c43316238.descon)
+	e2:SetTarget(c43316238.destg)
+	e2:SetOperation(c43316238.desop)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_REMOVE)
 	c:RegisterEffect(e3)
 end
-function c101005082.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c43316238.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return not c:IsPublic() and c:GetFlagEffect(101005082)==0 end
-	c:RegisterFlagEffect(101005082,RESET_CHAIN,0,1)
+	if chk==0 then return not c:IsPublic() and c:GetFlagEffect(43316238)==0 end
+	c:RegisterFlagEffect(43316238,RESET_CHAIN,0,1)
 end
-function c101005082.spfilter(c,e,tp)
-	return c:IsCode(101005082) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c43316238.spfilter(c,e,tp)
+	return c:IsCode(43316238) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c101005082.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101005082.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.IsPlayerCanDraw(tp) end
+function c43316238.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c43316238.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.IsPlayerCanDraw(tp) end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
 end
-function c101005082.spop(e,tp,eg,ep,ev,re,r,rp)
+function c43316238.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
 	if #g<1 then return end
 	Duel.ShuffleHand(tp)
@@ -48,26 +48,26 @@ function c101005082.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=g:Select(1-tp,1,1,nil)
 	Duel.BreakEffect()
 	Duel.SendtoGrave(tc,REASON_EFFECT+REASON_DISCARD)
-	if not tc:GetFirst():IsCode(101005082) then
+	if not tc:GetFirst():IsCode(43316238) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sc=Duel.SelectMatchingCard(tp,c101005082.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+		local sc=Duel.SelectMatchingCard(tp,c43316238.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 		if #sc>0 and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) then
 			Duel.Draw(tp,1,REASON_EFFECT)
 		end
 	end
 end
-function c101005082.descon(e,tp,eg,ep,ev,re,r,rp)
+function c43316238.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetPreviousLocation()==LOCATION_HAND and (r&REASON_DISCARD)~=0
 end
-function c101005082.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c43316238.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c101005082.desop(e,tp,eg,ep,ev,re,r,rp)
+function c43316238.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
