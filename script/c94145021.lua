f0b90687b1277c03bfa9e6d87de47814b298d3bc
AlphaKretin <deewhydeeecks@gmail.com>
2018-07-27 11:41:22 +0930

    Fix "Droll & Lock Bird"
    Must be in hand to activate

---
+++ script/c94145021.lua
@@ -0,0 +1,62 @@
+--ドロール＆ロックバード
+function c94145021.initial_effect(c)
+	--disable
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(94145021,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CUSTOM+94145021)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c94145021.condition)
+	e1:SetCost(c94145021.cost)
+	e1:SetOperation(c94145021.operation)
+	c:RegisterEffect(e1)
+	if not c94145021.global_check then
+		c94145021.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_TO_HAND)
+		ge1:SetCondition(c94145021.regcon)
+		ge1:SetOperation(c94145021.regop)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+function c94145021.cfilter(c,tp)
+	return c:IsControler(tp) and c:IsPreviousLocation(LOCATION_DECK)
+end
+function c94145021.regcon(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetCurrentPhase()==PHASE_DRAW then return false end
+	local v=0
+	if eg:IsExists(c94145021.cfilter,1,nil,0) then v=v+1 end
+	if eg:IsExists(c94145021.cfilter,1,nil,1) then v=v+2 end
+	if v==0 then return false end
+	e:SetLabel(({0,1,PLAYER_ALL})[v])
+	return true
+end
+function c94145021.regop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RaiseEvent(eg,EVENT_CUSTOM+94145021,re,r,rp,ep,e:GetLabel())
+end
+function c94145021.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ev==1-tp or ev==PLAYER_ALL
+end
+function c94145021.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsLocation(LOCATION_HAND) and c:IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(c,REASON_COST)
+end
+function c94145021.operation(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_TO_HAND)
+	e1:SetTargetRange(LOCATION_DECK,LOCATION_DECK)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_DRAW)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetTargetRange(1,1)
+	Duel.RegisterEffect(e2,tp)
+end
