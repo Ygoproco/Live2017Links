180319c358442911b7b6354189b1a1385fc273b6
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-08-06 23:21:04 -0300

    small update to card (#1067)
    small update to card

--- script/c1426714.lua
+++ script/c1426714.lua
@@ -24,7 +24,7 @@ function c1426714.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
 	e2:SetCondition(c1426714.atkcon)
-	e2:SetValue(aux.imval1)
+	e2:SetValue(aux.imval2)
 	c:RegisterEffect(e2)
 	--atk
 	local e3=Effect.CreateEffect(c)
