2537abddb2f89c21140595dee831e6b6017ec043
EerieCode <eeriecode@gmail.com>
2018-08-11 16:06:38 +0200

    Official IDs
    Super Armored Robot Weapon - Black Iron "C"

--- script/c77754169.lua
+++ script/c77754169.lua
@@ -1,43 +1,43 @@
 --超装甲兵器ロボ ブラックアイアンG
 --Super Armored Robot Weapon - Black Iron "C"
 --Script by dest
-function c100204007.initial_effect(c)
+function c77754169.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100204007,0))
+	e1:SetDescription(aux.Stringid(77754169,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,100204007)
-	e1:SetTarget(c100204007.sptg)
-	e1:SetOperation(c100204007.spop)
+	e1:SetCountLimit(1,77754169)
+	e1:SetTarget(c77754169.sptg)
+	e1:SetOperation(c77754169.spop)
 	c:RegisterEffect(e1)
 	--Destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100204007,1))
+	e2:SetDescription(aux.Stringid(77754169,1))
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,100204007+100)
-	e2:SetCost(c100204007.descost)
-	e2:SetTarget(c100204007.destg)
-	e2:SetOperation(c100204007.desop)
+	e2:SetCountLimit(1,77754170)
+	e2:SetCost(c77754169.descost)
+	e2:SetTarget(c77754169.destg)
+	e2:SetOperation(c77754169.desop)
 	c:RegisterEffect(e2)
 end
-function c100204007.eqfilter(c,tp)
+function c77754169.eqfilter(c,tp)
 	return c:IsRace(RACE_INSECT) and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,2,c,c:GetCode())
 end
-function c100204007.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100204007.eqfilter(chkc,tp) end
+function c77754169.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c77754169.eqfilter(chkc,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100204007.eqfilter,tp,LOCATION_GRAVE,0,1,nil,tp)
+		and Duel.IsExistingTarget(c77754169.eqfilter,tp,LOCATION_GRAVE,0,1,nil,tp)
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	local ct=math.min((Duel.GetLocationCount(tp,LOCATION_SZONE)),3)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c100204007.eqfilter,tp,LOCATION_GRAVE,0,1,ct,nil,tp)
+	local g=Duel.SelectTarget(tp,c77754169.eqfilter,tp,LOCATION_GRAVE,0,1,ct,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,g:GetCount(),0,0)
 end
-function c100204007.spop(e,tp,eg,ep,ev,re,r,rp)
+function c77754169.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local c=e:GetHandler()
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
@@ -52,35 +52,35 @@ function c100204007.spop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
 			e1:SetCode(EFFECT_EQUIP_LIMIT)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			e1:SetValue(c100204007.eqlimit)
+			e1:SetValue(c77754169.eqlimit)
 			tc:RegisterEffect(e1)
-			tc:RegisterFlagEffect(100204007,RESET_EVENT+RESETS_STANDARD,0,1)
+			tc:RegisterFlagEffect(77754169,RESET_EVENT+RESETS_STANDARD,0,1)
 		end
 	end
 end
-function c100204007.eqlimit(e,c)
+function c77754169.eqlimit(e,c)
 	return e:GetOwner()==c
 end
-function c100204007.tgfilter(c,tp)
-	return c:GetFlagEffect(100204007)~=0 and c:IsAbleToGraveAsCost()
-		and Duel.IsExistingMatchingCard(c100204007.desfilter,tp,0,LOCATION_MZONE,1,nil,c:GetTextAttack())
+function c77754169.tgfilter(c,tp)
+	return c:GetFlagEffect(77754169)~=0 and c:IsAbleToGraveAsCost()
+		and Duel.IsExistingMatchingCard(c77754169.desfilter,tp,0,LOCATION_MZONE,1,nil,c:GetTextAttack())
 end
-function c100204007.desfilter(c,atk)
+function c77754169.desfilter(c,atk)
 	return c:IsFaceup() and c:IsAttackAbove(atk)
 end
-function c100204007.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetEquipGroup():IsExists(c100204007.tgfilter,1,nil,tp) end
+function c77754169.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetEquipGroup():IsExists(c77754169.tgfilter,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=e:GetHandler():GetEquipGroup():FilterSelect(tp,c100204007.tgfilter,1,1,nil,tp)
+	local g=e:GetHandler():GetEquipGroup():FilterSelect(tp,c77754169.tgfilter,1,1,nil,tp)
 	e:SetLabel(g:GetFirst():GetTextAttack())
 	Duel.SendtoGrave(g,REASON_COST)
 end
-function c100204007.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c77754169.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(c100204007.desfilter,tp,0,LOCATION_MZONE,nil,e:GetLabel())
+	local g=Duel.GetMatchingGroup(c77754169.desfilter,tp,0,LOCATION_MZONE,nil,e:GetLabel())
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
-function c100204007.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c100204007.desfilter,tp,0,LOCATION_MZONE,nil,e:GetLabel())
+function c77754169.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c77754169.desfilter,tp,0,LOCATION_MZONE,nil,e:GetLabel())
 	Duel.Destroy(g,REASON_EFFECT)
 end
