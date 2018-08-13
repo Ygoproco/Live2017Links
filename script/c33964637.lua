7bd9ff843aee5d681159e6c28fed5d36cfb8395d
AlphaKretin <deewhydeeecks@gmail.com>
2018-08-13 15:23:27 +0930

    Fix "Secret Six Samurai - Rihan"
    Missing "revive limit" - could be revived without proper Special Summon

---
+++ script/c33964637.lua
@@ -0,0 +1,90 @@
+--影六武衆－リハン
+--Shadow Six Samurai – Rihan
+function c33964637.initial_effect(c)
+	--Fusion procedure
+  c:EnableReviveLimit()
+	aux.AddFusionProcMixN(c,true,true,c33964637.ffilter,3)
+	aux.AddContactFusion(c,c33964637.contactfil,c33964637.contactop,c33964637.splimit)
+	--fusion limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--remove
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100419006,0))
+	e4:SetCategory(CATEGORY_REMOVE)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCost(c33964637.rmcost)
+	e4:SetTarget(c33964637.rmtg)
+	e4:SetOperation(c33964637.rmop)
+	c:RegisterEffect(e4)	
+	--destroy replace
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EFFECT_DESTROY_REPLACE)
+	e5:SetRange(LOCATION_GRAVE)
+	e5:SetTarget(c33964637.reptg)
+	e5:SetValue(c33964637.repval)
+	e5:SetOperation(c33964637.repop)
+	c:RegisterEffect(e5)
+end
+c33964637.material_setcode=0x3d
+function c33964637.ffilter(c,fc,sumtype,tp,sub,mg,sg)
+	return c:IsFusionSetCard(0x3d) and c:GetAttribute(fc,sumtype,tp)~=0 and (not sg or not sg:IsExists(c33964637.fusfilter,1,c,c:GetAttribute(fc,sumtype,tp),fc,sumtype,tp))
+end
+function c33964637.contactfil(tp)
+	return Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,nil)
+end
+function c33964637.contactop(g)
+	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+function c33964637.fusfilter(c,attr,fc,sumtype,tp)
+	return c:IsAttribute(attr,fc,sumtype,tp) and not c:IsHasEffect(511002961)
+end
+function c33964637.splimit(e,se,sp,st)
+	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+end
+function c33964637.costfilter(c)
+	return c:IsSetCard(0x3d) and c:IsAbleToRemoveAsCost() and ((c:IsOnField() and c:IsFaceup()) or c:IsLocation(LOCATION_HAND)) 
+		and Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
+end
+function c33964637.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c33964637.costfilter,tp,LOCATION_ONFIELD+LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c33964637.costfilter,tp,LOCATION_ONFIELD+LOCATION_HAND,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c33964637.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToRemove() end
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function c33964637.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function c33964637.repfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x3d) and not c:IsReason(REASON_REPLACE)
+		and c:IsOnField() and c:IsControler(tp) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+end
+function c33964637.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),69832741) and e:GetHandler():IsAbleToRemove() 
+		and eg:IsExists(c33964637.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+function c33964637.repval(e,c)
+	return c33964637.repfilter(c,e:GetHandlerPlayer())
+end
+function c33964637.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
