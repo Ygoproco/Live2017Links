d1f2559edade95950ee740d4ea5294fdc8208c7c
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-08-14 11:37:25 -0300

    Cyberse Magician's effect range update

--- script/c24731391.lua
+++ script/c24731391.lua
@@ -5,7 +5,7 @@ function c24731391.initial_effect(c)
 	--halve damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_FZONE)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
