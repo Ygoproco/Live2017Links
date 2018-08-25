2dc9a22aa71053bbefb6cf0abf6100e96c2ee279
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-08-24 21:32:32 -0300

    small fix for ancient telescope
    interactions

--- script/c17092736.lua
+++ script/c17092736.lua
@@ -3,7 +3,6 @@ function c17092736.initial_effect(c)
     --confirm
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetTarget(c17092736.cftg)
     e1:SetOperation(c17092736.cfop)
@@ -16,12 +15,10 @@ end
 function c17092736.cfop(e,tp,eg,ep,ev,re,r,rp)
     local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
     local ct=math.min(5,Duel.GetFieldGroupCount(p,0,LOCATION_DECK))
-    local t={}
-    for i=1,ct do
-        t[i]=i
-    end
-    Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(17092736,0))
-    local g=Duel.GetDecktopGroup(1-p,ct)
+    if ct==0 then return end
+    Duel.Hint(HINT_SELECTMSG,p,HINTMSG_CONFIRM)
+    local ac=Duel.AnnounceLevel(p,1,ct)
+    local g=Duel.GetDecktopGroup(1-p,ac)
     if g:GetCount()>0 then
         Duel.ConfirmCards(p,g)
     end
