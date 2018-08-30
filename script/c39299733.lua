e2406f3542ca6675dd182e1271ffd1d5ff79855b
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-08-29 23:54:26 -0300

    small fix for Fortune Chariot
    interaction with other card's

--- script/c39299733.lua
+++ script/c39299733.lua
@@ -17,20 +17,22 @@ function c39299733.initial_effect(c)
 	c:RegisterEffect(e2)
 	--damage reduce
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_EQUIP+EFFECT_TYPE_CONTINUOUS)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e3:SetRange(LOCATION_SZONE)
 	e3:SetCondition(c39299733.rdcon)
 	e3:SetOperation(c39299733.rdop)
 	c:RegisterEffect(e3)
 end
 function c39299733.rdcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and Duel.GetAttackTarget()==nil
-		and e:GetHandler():IsHasEffect(EFFECT_DIRECT_ATTACK)
+	return ep~=tp and Duel.GetAttacker()==e:GetHandler():GetEquipTarget() and Duel.GetAttackTarget()==nil
+		and e:GetHandler():GetEquipTarget() and e:GetHandler():GetEquipTarget():IsHasEffect(EFFECT_DIRECT_ATTACK)
 		and Duel.IsExistingMatchingCard(aux.NOT(Card.IsHasEffect),tp,0,LOCATION_MZONE,1,nil,EFFECT_IGNORE_BATTLE_TARGET)
 end
 function c39299733.rdop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local effs={c:GetCardEffect(EFFECT_DIRECT_ATTACK)}
+	local tc=c:GetEquipTarget()
+	local effs={tc:GetCardEffect(EFFECT_DIRECT_ATTACK)}
 	local eg=Group.CreateGroup()
 	for _,eff in ipairs(effs) do
 		eg:AddCard(eff:GetOwner())
@@ -38,7 +40,6 @@ function c39299733.rdop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EFFECT)
 	local ec = #eg==1 and eg:GetFirst() or eg:Select(tp,1,1,nil):GetFirst()
 	if c==ec then
-		Duel.ChangeBattleDamage(ep,Duel.GetBattleDamage(ep)/2)
+		Duel.HalfBattleDamage(ep)
 	end
 end
-
