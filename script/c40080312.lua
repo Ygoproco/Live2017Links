849fe4e1fca6ff9530c75a104f7fd7224b42597e
EerieCode <eeriecode@gmail.com>
2018-09-02 12:20:34 +0200

    TCG update
    2018 Mega-Tins

--- script/c40080312.lua
+++ script/c40080312.lua
@@ -1,63 +1,63 @@
 --Elemental HERO Nebula Neos
-function c100244001.initial_effect(c)
+function c40080312.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcMix(c,true,true,89943723,43237273,80344569)
-	aux.AddContactFusion(c,c100244001.contactfil,c100244001.contactop,c100244001.splimit)
+	aux.AddContactFusion(c,c40080312.contactfil,c40080312.contactop,c40080312.splimit)
 	--return
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100244001,0))
+	e1:SetDescription(aux.Stringid(40080312,0))
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c100244001.retcon1)
-	e1:SetTarget(c100244001.rettg)
-	e1:SetOperation(c100244001.retop)
+	e1:SetCondition(c40080312.retcon1)
+	e1:SetTarget(c40080312.rettg)
+	e1:SetOperation(c40080312.retop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(0)
-	e2:SetCondition(c100244001.retcon2)
+	e2:SetCondition(c40080312.retcon2)
 	c:RegisterEffect(e2)
 	--draw
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100244001,0))
+	e3:SetDescription(aux.Stringid(40080312,1))
 	e3:SetCategory(CATEGORY_DRAW+CATEGORY_DISABLE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetCondition(c100244001.drcon)
-	e3:SetTarget(c100244001.drtg)
-	e3:SetOperation(c100244001.drop)
+	e3:SetCondition(c40080312.drcon)
+	e3:SetTarget(c40080312.drtg)
+	e3:SetOperation(c40080312.drop)
 	c:RegisterEffect(e3)
 end
-c100244001.listed_names={89943723,43237273,80344569}
-c100244001.material_setcode={0x8,0x3008,0x9,0x1f}
-function c100244001.contactfil(tp)
+c40080312.listed_names={89943723,43237273,80344569}
+c40080312.material_setcode={0x8,0x3008,0x9,0x1f}
+function c40080312.contactfil(tp)
 	return Duel.GetMatchingGroup(Card.IsAbleToDeckOrExtraAsCost,tp,LOCATION_ONFIELD,0,nil)
 end
-function c100244001.contactop(g,tp)
+function c40080312.contactop(g,tp)
 	Duel.ConfirmCards(1-tp,g)
 	Duel.SendtoDeck(g,nil,2,REASON_COST+REASON_MATERIAL)
 end
-function c100244001.splimit(e,se,sp,st)
+function c40080312.splimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA)
 end
-function c100244001.retcon1(e,tp,eg,ep,ev,re,r,rp)
+function c40080312.retcon1(e,tp,eg,ep,ev,re,r,rp)
 	return not e:GetHandler():IsHasEffect(42015635)
 end
-function c100244001.retcon2(e,tp,eg,ep,ev,re,r,rp)
+function c40080312.retcon2(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsHasEffect(42015635)
 end
-function c100244001.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c40080312.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
 end
-function c100244001.retop(e,tp,eg,ep,ev,re,r,rp)
+function c40080312.retop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
 	Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
@@ -66,16 +66,16 @@ function c100244001.retop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Remove(g,POS_FACEDOWN,REASON_EFFECT)
 	end
 end
-function c100244001.drcon(e,tp,eg,ep,ev,re,r,rp)
+function c40080312.drcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_EXTRA)
 end
-function c100244001.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c40080312.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,ct)
 end
-function c100244001.drop(e,tp,eg,ep,ev,re,r,rp)
+function c40080312.drop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
 	local d=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
