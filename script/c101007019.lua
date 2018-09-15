ea5d7c77e593a8635943a6afae8de4bcce880f0a
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-09-15 19:46:28 -0300

    fix for Shiranui Spearitualist (#1198)
    fix for Shiranui Spearitualist

--- script/c101007019.lua
+++ script/c101007019.lua
@@ -35,7 +35,7 @@ end
 function c101007019.sumop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c101007019.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c101007019.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
