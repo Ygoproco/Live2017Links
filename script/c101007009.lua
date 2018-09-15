4e773eabc568c593a26c0ef277ea8838731bea41
senpaizuri3 <senpaizuri3@gmail.com>
2018-09-15 14:21:48 -0400

    Small fix "T.G. Screw Serpent" (#1192)
    Small fix "T.G. Screw Serpent"

--- script/c101007009.lua
+++ script/c101007009.lua
@@ -26,7 +26,7 @@ function c101007009.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function c101007009.spfilter(c,e,tp)
-	return c:IsSetCard(0x27) and not c:IsCode(101007009) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x27) and c:GetLevel()<=4 and not c:IsCode(101007009) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101007009.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101007009.spfilter(chkc,e,tp) end
