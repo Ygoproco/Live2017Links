c6856120741c4604f48182e89ca5db9ee256a2c6
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-09-18 09:04:01 -0300

    small fix for Ninjitsu Art of Mirage-Transformation (#1211)
    was missing CARD TARGET property.

--- script/c63626024.lua
+++ script/c63626024.lua
@@ -3,6 +3,7 @@
 function c63626024.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
