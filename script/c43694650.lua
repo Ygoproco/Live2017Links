15a3786ee9ee36b742683b56b03844b6c242962e
senpaizuri3 <senpaizuri3@gmail.com>
2018-09-27 13:30:55 -0400

    Small fixes "Danger!? Jackalope?" "Danger! Nessie!" "Danger! Chupacabra!" (#1242)
    * Small fix "Danger!? Jackalope?"
    
    * Small fix "Danger! Nessie!"
    
    * Small fix "Danger! Chupacabra!"

--- script/c43694650.lua
+++ script/c43694650.lua
@@ -37,7 +37,7 @@ function c43694650.spfilter(c,e,tp)
 	return c:IsCode(43694650) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c43694650.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(c43694650.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.IsPlayerCanDraw(tp) end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
 end
