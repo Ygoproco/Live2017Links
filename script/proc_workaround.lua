5f31b0bdcddef98543008ff5317927448746594c
EerieCode <eeriecode@gmail.com>
2018-10-04 22:08:58 +0200

    Added new cards
    Consolation Prize
    Danger! Dogman!
    Danger! Mothman!
    Danger!? Tsuchinoko?
    Danger! Response Team
    Second Expedition into Danger!
    Noble Knight Iyvanne
    Morgan, the Enchantress of Avalon
    Heritage of the Chalice
    Until Noble Arms are Needed Once Again
    T.G. Tank Larva
    Witching Hour

--- script/proc_workaround.lua
+++ script/proc_workaround.lua
@@ -253,3 +253,15 @@ Duel.NegateSummon=function(g)
 		Duel.RaiseEvent(ng,EVENT_CUSTOM+101007179,Effect.GlobalEffect(),0,0,0,0)
 	end
 end
+
+--T.G. Tank Larva
+local nt=Card.IsNotTuner
+Card.IsNotTuner=function (c,sc,tp)
+    local nte = c:GetCardEffect(EFFECT_NONTUNER)
+    local val = nte and nte:GetValue() or nil
+    if not val or type(val) == 'number' then
+        return nt(c,sc,tp)
+    else
+        return val(c,sc,tp) or not c:IsType(TYPE_TUNER,sc,SUMMON_TYPE_SYNCHRO,tp)
+    end
+end
