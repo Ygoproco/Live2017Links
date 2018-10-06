31c3deba5d55971e1a244a9cf54dfbb581e3185e
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-10-06 07:19:02 -0300

    small clean up (#1285)
    small clean up

--- script/c100411003.lua
+++ script/c100411003.lua
@@ -40,7 +40,6 @@ function c100411003.spcon(e,tp,eg,ep,ev,re,r,rp)
 	if eg:IsExists(c100411003.cfilter,1,nil,tp) then
 		local tc=eg:GetFirst()
 		e:SetLabel(tc:GetOriginalCode())
-		Debug.Message(Duel.IsExistingMatchingCard(c100411003.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,tc:GetOriginalCode()))
 		return Duel.IsExistingMatchingCard(c100411003.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,tc:GetOriginalCode())
 	end
 end
