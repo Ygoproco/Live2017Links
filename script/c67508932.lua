7aedd0ef7d5729b86ed850730c9183598e529ae9
EerieCode <eeriecode@gmail.com>
2018-10-08 22:25:33 +0200

    Official passcodes
    Timelord Progenitor Vulgate

--- script/c67508932.lua
+++ script/c67508932.lua
@@ -1,6 +1,6 @@
 --時械神祖ヴルガータ
 --Timelord Progenitor Vulgate
-function c100233001.initial_effect(c)
+function c67508932.initial_effect(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,nil,1,1,aux.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
@@ -20,74 +20,74 @@ function c100233001.initial_effect(c)
 	c:RegisterEffect(e3)
 	--banish
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(100233001,0))
+	e4:SetDescription(aux.Stringid(67508932,0))
 	e4:SetCategory(CATEGORY_REMOVE)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_DAMAGE_STEP_END)
-	e4:SetCondition(c100233001.rmcond)
-	e4:SetTarget(c100233001.rmtg)
-	e4:SetOperation(c100233001.rmop)
+	e4:SetCondition(c67508932.rmcond)
+	e4:SetTarget(c67508932.rmtg)
+	e4:SetOperation(c67508932.rmop)
 	c:RegisterEffect(e4)
 	--special summon
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(100233001,1))
+	e5:SetDescription(aux.Stringid(67508932,1))
 	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e5:SetCode(EVENT_PHASE+PHASE_END)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetCountLimit(1)
-	e5:SetCondition(c100233001.spcon)
-	e5:SetTarget(c100233001.sptg)
-	e5:SetOperation(c100233001.spop)
+	e5:SetCondition(c67508932.spcon)
+	e5:SetTarget(c67508932.sptg)
+	e5:SetOperation(c67508932.spop)
 	c:RegisterEffect(e5)
 end
-function c100233001.rmcond(e)
+function c67508932.rmcond(e)
 	return e:GetHandler():GetSummonLocation()&LOCATION_EXTRA==LOCATION_EXTRA
 end
-function c100233001.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c67508932.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,nil) end
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,LOCATION_MZONE,1-tp)
 end
-function c100233001.rmop(e,tp,eg,ep,ev,re,r,rp)
+function c67508932.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,nil)
 	if Duel.Remove(g,0,REASON_EFFECT)>0 then
 		local og=Duel.GetOperatedGroup()
 		for oc in aux.Next(og) do
-			oc:RegisterFlagEffect(100233001,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1,c:GetFieldID())
+			oc:RegisterFlagEffect(67508932,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1,c:GetFieldID())
 		end
-		c:RegisterFlagEffect(100233101,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		c:RegisterFlagEffect(67508933,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 	end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e1:SetCondition(c100233001.dcon)
-	e1:SetOperation(c100233001.dop)
+	e1:SetCondition(c67508932.dcon)
+	e1:SetOperation(c67508932.dop)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
-function c100233001.dcon(e,tp,eg,ep,ev,re,r,rp)
+function c67508932.dcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep~=tp
 end
-function c100233001.dop(e,tp,eg,ep,ev,re,r,rp)
+function c67508932.dop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.HalfBattleDamage(ep)
 end
-function c100233001.spfilter(c,e,tp)
-	return c:GetFlagEffectLabel(100233001)==e:GetHandler():GetFieldID()
+function c67508932.spfilter(c,e,tp)
+	return c:GetFlagEffectLabel(67508932)==e:GetHandler():GetFieldID()
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100233001.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(100233101)>0
+function c67508932.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(67508933)>0
 end
-function c100233001.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c67508932.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(c100233001.spfilter,tp,0,LOCATION_REMOVED,nil,e,tp)
+	local g=Duel.GetMatchingGroup(c67508932.spfilter,tp,0,LOCATION_REMOVED,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,#g,tp,LOCATION_REMOVED)
 end
-function c100233001.spop(e,tp,eg,ep,ev,re,r,rp)
+function c67508932.spop(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	local tg=Duel.GetMatchingGroup(c100233001.spfilter,tp,0,LOCATION_REMOVED,nil,e,tp)
+	local tg=Duel.GetMatchingGroup(c67508932.spfilter,tp,0,LOCATION_REMOVED,nil,e,tp)
 	if ft<=0 or #tg==0 then return end
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
