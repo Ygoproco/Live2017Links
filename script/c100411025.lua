fe1eb6c031bbe886ce5b93a034862866f9d7ac01
EerieCode <eeriecode@gmail.com>
2018-10-09 21:51:37 +0200

    Added new cards
    White Rose Dragon
    Red Rose Dragon

---
+++ script/c100411025.lua
@@ -0,0 +1,51 @@
+--レッドローズ・ドラゴン
+--Red Rose Dragon
+--Scripted by AlphaKretin
+local s,id=GetID()
+local CODE_FROZEN_ROAR
+local CODE_BLOOMING_ROSE
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BE_MATERIAL)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler(e,tp,eg,ep,ev,re,r,rp)
+	return c:IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x223) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+	local rc=e:GetHandler():GetReasonCard()
+	if rc and (rc:IsCode(73580471) or (rc:IsRace(RACE_PLANT) and rc:IsType(TYPE_SYNCHRO)) then
+		e:SetLabel(1)
+	end
+end
+function s.thfilter(c)
+	return c:IsCode(CODE_FROZEN_ROAR,CODE_BLOOMING_ROSE) and c:IsAbleToHand()
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>1 and e:GetLabel()==1 
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local g2=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g2>0 then
+			Duel.SendtoHand(g2,tp,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g2)
+		end
+	end
+end
