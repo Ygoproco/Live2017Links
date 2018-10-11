9d8e836032e81678dca98214a0d6e60e5fba9558
EerieCode <eeriecode@gmail.com>
2018-10-11 21:20:59 +0200

    Added new cards
    Iustia the Guardragon
    Garmides the Guardragon
    Prominesis the Guardragon
    Andrake the Guardragon
    Elpy the Guardragon
    Pisti the Guardragon
    Agarpain the Guardragon
    Guardragon's Barrier
    Guardragon of the World Legacy
    Guardragon’s Awakening
    Guardragon of Corruption

--- script/utility.lua
+++ script/utility.lua
@@ -1178,6 +1178,29 @@ function Auxiliary.NeosReturnOperation(c,extraop)
 	end
 end
 
+--Returns the zones, on the specified player's field, pointed by the specified number of Link markers. Includes Extra Monster Zones.
+function Duel.GetZoneWithLinkedCount(count,tp)
+	local g = Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_MZONE,LOCATION_MZONE,nil,TYPE_LINK)
+	local zones = {}
+	local z = {0x1,0x2,0x4,0x8,0x10,0x20,0x40}
+	for _,zone in ipairs(z) do
+		local ct = 0
+		for tc in aux.Next(g) do
+			if (zone&tc:GetLinkedZone(tp))~= 0 then
+				ct = ct + 1
+			end
+		end
+		zones[zone] = ct
+	end
+	local rzone = 0
+	for i,ct in pairs(zones) do
+		if ct >= count then
+			rzone = rzone | i
+		end
+	end
+	return rzone
+end
+
 function loadutility(file)
 	local f1 = loadfile("expansions/live2017links/script/"..file)
 	local f2 = loadfile("expansions/script/"..file)
