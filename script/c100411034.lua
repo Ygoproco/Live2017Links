e73ee46402998fd6e1c6102c259c7a9455f94761
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2018-10-16 02:39:13 -0400

    Updated rank 11 EARTH machine Xyz
    Should only attack multiple times on monsters.

--- script/c100411034.lua
+++ script/c100411034.lua
@@ -19,7 +19,7 @@ function s.initial_effect(c)
     --multi attack
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_EXTRA_ATTACK)
+    e2:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
     e2:SetValue(s.raval)
     c:RegisterEffect(e2)
 end
