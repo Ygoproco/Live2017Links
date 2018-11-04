d832d9356394e4c0b9c752726200075090dccbf8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2018-11-04 13:41:17 +0200

    "Mutant Mindmaster" update (#1402)
    "Mutant Mindmaster" update

--- script/c11508758.lua
+++ script/c11508758.lua
@@ -13,10 +13,10 @@ function c11508758.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c11508758.ctlcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetAttackTarget()~=nil
+	return Duel.GetAttackTarget()~=nil and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>1
 end
 function c11508758.filter(c)
-	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsControlerCanBeChanged()
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsControlerCanBeChanged() and c:IsAttackable()
 end
 function c11508758.ctltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c11508758.filter(chkc) end
@@ -28,13 +28,28 @@ end
 function c11508758.ctlop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		Duel.AdjustInstantly(tc)
 		if Duel.GetControl(tc,tp,PHASE_BATTLE,1)~=0 then
 			if tc:IsAttackable() and not tc:IsImmuneToEffect(e) then
 				local ats=tc:GetAttackableTarget()
 				Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(11508758,1))
-				local g=ats:Select(tp,1,1,nil)
-				Duel.CalculateDamage(tc,g:GetFirst())
+				if #ats>0 then
+					local g=ats:Select(tp,1,1,nil)
+					Duel.CalculateDamage(tc,g:GetFirst())
+				end
 			end
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_CANNOT_ATTACK)
+			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE)
+			e:GetHandler():RegisterEffect(e2,true)
 		end
 	end
 end
