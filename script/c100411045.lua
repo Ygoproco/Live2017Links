3ae03715e347c495a9da509540afc3a41c180745
AlphaKretin <deewhydeeecks@gmail.com>
2018-11-09 10:39:02 +1030

    Fix "Lunalight Sabre Dancer"
    Missing flag on ATK gain effect

--- script/c100411045.lua
+++ script/c100411045.lua
@@ -7,6 +7,7 @@ function s.initial_effect(c)
 	--atk up
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(s.atkval)
