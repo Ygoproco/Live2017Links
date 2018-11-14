8a0d11e2c9ace18623da4be34b76fec248b65d4e
EerieCode <eeriecode@gmail.com>
2018-11-14 21:35:30 +0100

    Updated old "Six Samurai" cards
    Added Auxiliary.IsCounterAdded

--- script/utility.lua
+++ script/utility.lua
@@ -1210,6 +1210,15 @@ function Duel.GetZoneWithLinkedCount(count,tp)
 	return rzone
 end
 
+--Checks whether a card has an effect that can add a certain type of counter
+function aux.IsCounterAdded(c,counter)
+	if not c.counter_add_list then return false end
+	for _,ccounter in ipairs(c.counter_add_list) do
+		if counter==ccounter then return true end
+	end
+	return false
+end
+
 function loadutility(file)
 	local f1 = loadfile("expansions/live2017links/script/"..file)
 	local f2 = loadfile("expansions/script/"..file)
