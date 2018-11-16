b7ee4269cd565c045f50327d5d76b60683771e52
EerieCode <eeriecode@gmail.com>
2018-11-16 22:36:42 +0100

    Fixed "Hi-Speedroid GOM Gun"

--- script/c100235081.lua
+++ script/c100235081.lua
@@ -81,7 +81,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
-	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon(c:GetLevel()),1,tp,HINTMSG_SELECT)
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon(e:GetLabel()),1,tp,HINTMSG_SELECT)
 	if #sg==2 then return end
 	Duel.ConfirmCards(1-tp,sg)
 	Duel.ShuffleDeck(tp)
