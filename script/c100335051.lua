e1f185a0bd6590ccf1bf4e905e8f64790751eb7f
mercury233 <me@mercury233.me>
2018-11-23 22:33:55 +0800

    format

--- script/c100335051.lua
+++ script/c100335051.lua
@@ -43,12 +43,12 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-	    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	    if #g>0 then
-	        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
-	    end
-	end
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+        end
+    end
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_CANNOT_ACTIVATE)
