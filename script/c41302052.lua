2d01043dab524849868ef4dd66e78dbdb5e01eaf
AlphaKretin <deewhydeeecks@gmail.com>
2018-12-09 21:53:44 +1030

    Fix conditional "Unaffected by card effects" monsters (#1497)
    Fix conditional "Unaffected by card effects" monsters

--- script/c41302052.lua
+++ script/c41302052.lua
@@ -1,7 +1,8 @@
 --トリックスター・ベラマドンナ
 --Trickstar Bella Madonna
 --Script by nekrozar
-function c41302052.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--link summon
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkSetCard,0xfb),2)
 	c:EnableReviveLimit()
@@ -11,46 +12,45 @@ function c41302052.initial_effect(c)
 	e1:SetCode(EFFECT_IMMUNE_EFFECT)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(c41302052.imcon)
-	e1:SetValue(c41302052.immval)
+	--e1:SetCondition(s.imcon) --handled in value for mid-resolution updating
+	e1:SetValue(s.immval)
 	c:RegisterEffect(e1)
 	--damage
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(41302052,0))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,41302052)
-	e2:SetCondition(c41302052.damcon)
-	e2:SetTarget(c41302052.damtg)
-	e2:SetOperation(c41302052.damop)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.damcon)
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
 end
-function c41302052.imcon(e)
+function s.imcon(e)
 	local c=e:GetHandler()
 	return c:IsSummonType(SUMMON_TYPE_LINK) and c:GetLinkedGroupCount()==0
 end
-function c41302052.immval(e,te)
-	return te:GetOwner()~=e:GetHandler() and te:IsActivated()
+function s.immval(e,te)
+	return te:GetOwner()~=e:GetHandler() and te:IsActivated() and s.imcon(e) --condition handling for mid-resolution updating
 end
-function c41302052.damcon(e,tp,eg,ep,ev,re,r,rp)
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetLinkedGroupCount()==0
 end
-function c41302052.damfilter(c)
+function s.damfilter(c)
 	return c:IsSetCard(0xfb) and c:IsType(TYPE_MONSTER)
 end
-function c41302052.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c41302052.damfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	local g=Duel.GetMatchingGroup(c41302052.damfilter,tp,LOCATION_GRAVE,0,nil)
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.damfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.damfilter,tp,LOCATION_GRAVE,0,nil)
 	local dam=g:GetClassCount(Card.GetCode)*200
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(dam)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
 end
-function c41302052.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local g=Duel.GetMatchingGroup(c41302052.damfilter,tp,LOCATION_GRAVE,0,nil)
+	local g=Duel.GetMatchingGroup(s.damfilter,tp,LOCATION_GRAVE,0,nil)
 	local dam=g:GetClassCount(Card.GetCode)*200
 	Duel.Damage(p,dam,REASON_EFFECT)
 end
-
