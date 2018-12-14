c6415579099dd0752f7321a8d9c463a7d918af93
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-12-14 20:53:15 -0200

    Update for "Prank-Kids Rocket Ride"
    Added Duel.HintSelection to register the targeted cards.

--- script/c18514525.lua
+++ script/c18514525.lua
@@ -1,36 +1,37 @@
 --プランキッズ・ロケット
---Prankids Rocket
+--Prank-Kids Rocket Ride
 --Scripted by Eerie Code
-function c18514525.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.AddFusionProcMixN(c,true,true,aux.FilterBoolFunction(Card.IsFusionSetCard,0x120),2)
 	--direct attack
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(18514525,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCountLimit(1,18514525)
-	e1:SetCondition(c18514525.atkcon)
-	e1:SetOperation(c18514525.atkop)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.atkcon)
+	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(18514525,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,18514526)
-	e2:SetCost(c18514525.spcost)
-	e2:SetTarget(c18514525.sptg)
-	e2:SetOperation(c18514525.spop)
+	e2:SetCountLimit(1,id+1)
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
-function c18514525.atkcon(e)
+function s.atkcon(e)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
-function c18514525.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local atk=c:GetAttack()
 	if not c:IsRelateToEffect(e) or atk<1000 then return end
@@ -48,29 +49,30 @@ function c18514525.atkop(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e2)
 	end
 end
-function c18514525.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
-function c18514525.spfilter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x120) and not c:IsType(TYPE_FUSION) 
 		and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c18514525.spcheck(sg,e,tp)
+function s.spcheck(sg,e,tp)
 	return sg:GetClassCount(Card.GetCode)==2
 end
-function c18514525.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	local g=Duel.GetMatchingGroup(c18514525.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
 	if chk==0 then return ft>1 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
-		and aux.SelectUnselectGroup(g,e,tp,2,2,c18514525.spcheck,0) end
-	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,c18514525.spcheck,1,tp,HINTMSG_SPSUMMON)
+		and aux.SelectUnselectGroup(g,e,tp,2,2,s.spcheck,0) end
+	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,s.spcheck,1,tp,HINTMSG_SPSUMMON)
 	Duel.SetTargetCard(tg)
+	Duel.HintSelection(tg)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tg,#tg,0,0)
 end
-function c18514525.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<g:GetCount() or (#g>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)) then return end
 	local tc=g:GetFirst()
