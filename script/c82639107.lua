7b35e71fd0f82433b6d2f92b2e54dabd04fb799a
senpaizuri3 <senpaizuri3@gmail.com>
2018-12-15 19:01:46 -0500

    Update c82639107.lua

--- script/c82639107.lua
+++ script/c82639107.lua
@@ -33,7 +33,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 		local sg2=g2:Select(tp,1,1,nil)
 		sg1:Merge(sg2)
-		if Duel.SendtoGrave(sg1,REASON_EFFECT)>0 then
+		if Duel.SendtoGrave(sg1,REASON_EFFECT)==2 then
 			Duel.ShuffleDeck(tp)
 			Duel.BreakEffect()
 			Duel.Draw(tp,2,REASON_EFFECT)
