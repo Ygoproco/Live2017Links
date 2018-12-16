8127351a93395ff2a88d81da6b5597ecd1ef8958
senpaizuri3 <senpaizuri3@gmail.com>
2018-12-16 14:19:45 -0500

    Small fix "Speedroid Marble Machine"

--- script/c100238007.lua
+++ script/c100238007.lua
@@ -40,6 +40,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
         e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
         e1:SetRange(LOCATION_MZONE)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        e1:SetValue(1)
         tc:RegisterEffect(e1)
     end
 end
