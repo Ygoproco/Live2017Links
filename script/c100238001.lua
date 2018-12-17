e62b27a712a815a1d8a8825aed37889bfab1d4aa
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-12-17 18:58:02 -0200

    Fix "Mighty Flame Swordsman" (#1537)
    * Fix "Mighty Flame Swordsman"
    
    Updated the Link Summon procedure to match the requirement of "2 monsters with different names"
    
    * Update c100238001.lua

--- script/c100238001.lua
+++ script/c100238001.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
-    aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),2,2,s.lcheck)
+    aux.AddLinkProcedure(c,nil,2,2,s.lcheck)
     --boost
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
