484415cb77021a4afb19969a4c99deb260d734b2
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-12-31 11:31:29 -0200

    Updated dicette's effect to match it text. (#1568)

--- script/c101008080.lua
+++ script/c101008080.lua
@@ -25,38 +25,41 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-	return tp==Duel.GetTurnPlayer()
+	return tp == Duel.GetTurnPlayer()
 end
 function s.filter(c)
 	return c:IsAbleToRemove() and aux.SpElimFilter(c)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,6,nil) end
+	if chk == 0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,6,nil) end
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local r=Duel.TossDice(tp,1)
+	local r = Duel.TossDice(tp,1)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local rg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,r,r,nil)
-	if #rg>0 then
-		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
-	end
-	if r==1 then
-		Duel.DiscardDeck(tp,6,REASON_EFFECT)
+	local rg = Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,r,r,nil)
+	if #rg > 0 then
+		local rm = Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
+		if rm == r and r == 1 then
+			Duel.BreakEffect()
+			Duel.DiscardDeck(tp,6,REASON_EFFECT)
+		end
 	end
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return tp~=Duel.GetTurnPlayer()
+	return tp ~= Duel.GetTurnPlayer()
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,6) end
+	if chk == 0 then return Duel.IsPlayerCanDiscardDeck(tp,6) end
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local r=Duel.TossDice(tp,1)
-	Duel.DiscardDeck(tp,r,REASON_EFFECT)
-	if r==6 then
-		local rg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,1,nil)
-		if #rg>0 then
-		   Duel.Remove(rg,POS_FACEUP,REASON_EFFECT) 
+	local r = Duel.TossDice(tp,1)
+	local dd = Duel.DiscardDeck(tp,r,REASON_EFFECT)
+	if r == 6 and dd == r then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local rg = Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,1,nil)
+		if #rg > 0 then
+			Duel.BreakEffect()
+			Duel.Remove(rg,POS_FACEUP,REASON_EFFECT) 
 		end
 	end
 end
