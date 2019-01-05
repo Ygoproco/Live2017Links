ca078f1067e925fa772f9ca08f12fcea660913fa
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-01-05 21:14:03 -0200

    Update c90809975.lua

--- script/c90809975.lua
+++ script/c90809975.lua
@@ -94,7 +94,8 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.NegateActivation(ev) then return end
 	if rc:IsRelateToEffect(re) and Duel.Destroy(eg,REASON_EFFECT)~=0 and not rc:IsLocation(LOCATION_HAND+LOCATION_DECK) 
 		and aux.nvfilter(rc) then
-		if ((rc:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp)>0) or (not rc:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0))
+		if (rc:IsType(TYPE_MONSTER) and
+			((rc:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp)>0) or (not rc:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0)))
 			and rc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
 			and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 			Duel.BreakEffect()
