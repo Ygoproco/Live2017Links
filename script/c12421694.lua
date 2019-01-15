96affc88305e35bfb322a64e2e8d56736e1b2621
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-01-15 18:17:57 -0200

    Fixed "Sky Striker Ace - Kaina"
    Fixed an incorrect Reset used for the effect that prevents a target from attacking

--- script/c12421694.lua
+++ script/c12421694.lua
@@ -38,7 +38,7 @@ function s.naop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_ATTACK)
-		e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
 		tc:RegisterEffect(e1)
 	end
 end
