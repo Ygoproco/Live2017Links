b0cc4aebf46afbe08e61ed57f546005777d652e2
EerieCode <eeriecode@gmail.com>
2019-01-15 21:05:19 +0100

    Added new cards
    Hi-Speedroid Kitedrake
    Exodia, Master of the Guard
    Red-Eyes Alternative Black Dragon
    Malefic Divide
    Cubic Omen

---
+++ script/c100236118.lua
@@ -0,0 +1,127 @@
+--方界縁起
+--Cubic Omen
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--counters
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetHintTiming(0,TIMING_BATTLE_START)
+	e2:SetCountLimit(1,id+100)
+	e2:SetCost(aux.bfgcost) 
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damact)
+	c:RegisterEffect(e2)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xe3)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ct=Duel.GetMatchingGroupCount(s.cfilter,tp,LOCATION_MZONE,0,nil)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	if ct>0 then
+		local sg=g:Select(tp,1,ct,nil)
+		local ac=sg:GetFirst()
+		while ac do
+		ac:AddCounter(0x1038,1)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetCondition(s.condition)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		ac:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE)
+		ac:RegisterEffect(e2)
+		ac=sg:GetNext()
+		end
+	end
+end
+function s.condition(e)
+	return e:GetHandler():GetCounter(0x1038)>0
+end
+function s.tgfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xe3)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and s.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.damact(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+		e1:SetCode(EVENT_LEAVE_FIELD)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetOperation(s.shchk)
+		e1:SetLabelObject(tc)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,2))
+		e2:SetCategory(CATEGORY_DAMAGE)
+		e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+		e2:SetCode(EVENT_BATTLE_DESTROYING)
+		e2:SetCondition(s.shcon)
+		e2:SetTarget(s.dmtg)
+		e2:SetOperation(s.damop)
+		e2:SetLabelObject(tc)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,3))
+		tc:RegisterEffect(e2)
+	end
+end
+function s.shchk(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	local bt=tc:GetBattleTarget()
+	for tc in aux.Next(eg) do
+		if bt==tc and tc:GetCounter(0x1038) > 0 then
+			tc:RegisterFlagEffect(id+2,RESET_CHAIN,0,1)
+		end
+	end
+end
+function s.shcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	local bt=tc:GetBattleTarget()
+	return tc and tc:GetFlagEffect(id)~=0 and bt and bt:GetFlagEffect(id+2) ~= 0
+end
+function s.dmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local tc=e:GetLabelObject():GetBattleTarget()
+	local atk=tc:GetBaseAttack()
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(atk)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
