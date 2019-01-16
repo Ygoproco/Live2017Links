04c32af7fb53d5ccf371ca0bddcb78bd2e1b125b
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-01-16 04:58:21 -0200

    fix for Rebirth of Nephthys (#1632)
    fix for Rebirth of Nephthys

--- script/c23459650.lua
+++ script/c23459650.lua
@@ -1,15 +1,20 @@
 --ネフティスの輪廻
 --Rebirth of Nephthys
-function c23459650.initial_effect(c)
-	aux.AddRitualProcGreater(c,c23459650.ritualfil,nil,nil,nil,nil,c23459650.stage2)
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddRitualProcGreater(c,s.ritualfil,nil,nil,nil,nil,s.stage2)
 end
-c23459650.fit_monster={88176533,24175232}
-function c23459650.ritualfil(c)
+s.fit_monster={88176533,24175232}
+function s.ritualfil(c)
 	return c:IsSetCard(0x11f) and c:IsRitualMonster()
 end
-function c23459650.stage2(mg,e,tp,eg,ep,ev,re,r,rp)
+function s.mfilter(c)
+	local code=c:GetPreviousCodeOnField()
+	return code == 88176533 or code == 24175232
+end
+function s.stage2(mg,e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
-	if mg:IsExists(c23459650.mfilter,1,nil) and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(23459650,0)) then
+	if mg:IsExists(s.mfilter,1,nil) and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 		local sg=g:Select(tp,1,1,e:GetHandler())
