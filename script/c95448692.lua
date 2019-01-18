255595d52489dd676e5ad51b7de2746a4785da1d
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-01-18 20:59:20 -0200

    small fix for damage diet (#1649)
    small fix for damage diet

--- script/c95448692.lua
+++ script/c95448692.lua
@@ -1,10 +1,11 @@
 --ダメージ・ダイエット
-function c95448692.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetOperation(c95448692.activate)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--effect damage
 	local e2=Effect.CreateEffect(c)
@@ -12,58 +13,47 @@ function c95448692.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCost(aux.bfgcost)
-	e2:SetOperation(c95448692.activate2)
+	e2:SetOperation(s.activate2)
 	c:RegisterEffect(e2)
 end
-c95448692[0]=0
-c95448692[1]=0
-function c95448692.activate(e,tp,eg,ep,ev,re,r,rp)
-	c95448692[tp]=1
-	if Duel.GetFlagEffect(tp,95448692)~=0 then return end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)~=0 then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
-	e1:SetValue(c95448692.val)
+	e1:SetValue(s.val)
 	e1:SetReset(RESET_PHASE+PHASE_END,1)
 	Duel.RegisterEffect(e1,tp)
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e2:SetCondition(c95448692.rdcon)
-	e2:SetOperation(c95448692.dop)
+	e2:SetCondition(s.rdcon)
+	e2:SetOperation(s.rdop)
 	e2:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e2,tp)
-	Duel.RegisterFlagEffect(tp,95448692,RESET_PHASE+PHASE_END,0,1)
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
 end
-function c95448692.activate2(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(tp,95448692)~=0 then return end
-	c95448692[tp]=0
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)~=0 then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
-	e1:SetValue(c95448692.val)
-	e1:SetReset(RESET_PHASE+PHASE_END,1)
-	Duel.RegisterEffect(e1,tp)
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e2:SetCondition(c95448692.rdcon)
-	e2:SetOperation(c95448692.dop)
+	e1:SetValue(s.val)
 	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-	Duel.RegisterFlagEffect(tp,95448692,RESET_PHASE+PHASE_END,0,1)
+	Duel.RegisterEffect(e1,tp)
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
 end
-function c95448692.rdcon(e,tp,eg,ep,ev,re,r,rp)
+function s.rdcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep==tp
 end
-function c95448692.rdop(e,tp,eg,ep,ev,re,r,rp)
+function s.rdop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.HalfBattleDamage(ep)
 end
-function c95448692.val(e,re,dam,r,rp,rc)
+function s.val(e,re,dam,r,rp,rc)
 	if bit.band(r,REASON_EFFECT)~=0 then
 		return dam/2
 	else return dam end
