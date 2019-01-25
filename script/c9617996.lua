0f64805e8da0fde4fa4a44306f292e92613b885e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-01-25 19:44:45 +0200

    "Crusadia Regulex" update
    Removed unused function.

--- script/c9617996.lua
+++ script/c9617996.lua
@@ -1,15 +1,16 @@
 --レグレクス・パラディオン
---Regulex Palladion
-function c9617996.initial_effect(c)
+--Crusadia Regulex
+local s,id=GetID()
+function s.initial_effect(c)
 	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2,2,c9617996.lcheck)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2,2,s.lcheck)
 	--atk up
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(c9617996.atkval)
+	e1:SetValue(s.atkval)
 	c:RegisterEffect(e1)
 	--cannot attack
 	local e2=Effect.CreateEffect(c)
@@ -17,54 +18,50 @@ function c9617996.initial_effect(c)
 	e2:SetCode(EFFECT_CANNOT_ATTACK)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(c9617996.atklimit)
+	e2:SetTarget(s.atklimit)
 	c:RegisterEffect(e2)
 	--Search
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(9617996,0))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCountLimit(1,9617996)
+	e3:SetCountLimit(1,id)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(c9617996.thcon)
-	e3:SetTarget(c9617996.thtg)
-	e3:SetOperation(c9617996.thop)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)	
 end
-function c9617996.lcheck(g,lc)
+function s.lcheck(g,lc)
 	return g:IsExists(Card.IsLinkSetCard,1,nil,0x116)
 end
-function c9617996.matfilter(c)
-	return c:IsLinkSetCard(0x116) and not c:IsLinkCode(3679218)
-end
-function c9617996.atkval(e,c)
+function s.atkval(e,c)
 	local g=e:GetHandler():GetLinkedGroup():Filter(Card.IsFaceup,nil) 
 	return g:GetSum(Card.GetBaseAttack)
 end
-function c9617996.atklimit(e,c)
+function s.atklimit(e,c)
 	return e:GetHandler():GetLinkedGroup():IsContains(c)
 end
-function c9617996.thcfilter(c,tp,lg)
+function s.thcfilter(c,tp,lg)
 	return c:IsType(TYPE_EFFECT) and lg:IsContains(c)
 end
-function c9617996.thcon(e,tp,eg,ep,ev,re,r,rp)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local lg=e:GetHandler():GetLinkedGroup()
-	return eg:IsExists(c9617996.thcfilter,1,nil,tp,lg)
+	return eg:IsExists(s.thcfilter,1,nil,tp,lg)
 end
-function c9617996.thfilter(c,tp)
+function s.thfilter(c,tp)
 	return (c:IsType(TYPE_SPELL) or c:IsType(TYPE_TRAP)) and c:IsSetCard(0x116) and c:IsAbleToHand()
 end
-function c9617996.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c9617996.thfilter,tp,LOCATION_DECK,0,1,nil,tp) end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c9617996.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g1=Duel.SelectMatchingCard(tp,c9617996.thfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
+	local g1=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
 	if g1:GetCount()>0 then
 		Duel.SendtoHand(g1,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g1)
 	end
 end
-
