0f60ed3a063f5b90ca94d823acbaf65e816ba6a3
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-02-01 17:04:47 -0200

    Fixed "Salamangreat Foxy"
    Incorrect usage of "IsAbleToGraveAsCost" would cause an incorrect interaction with Macro Cosmos

--- script/c94620082.lua
+++ script/c94620082.lua
@@ -1,63 +1,62 @@
 --転生炎獣フォクシー
 --Salamangreat Foxy
 --Scripted by AlphaKretin
-function c94620082.initial_effect(c)
+function s.initial_effect(c)
 	--to hand
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(94620082,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCountLimit(1,94620082)
-	e1:SetTarget(c94620082.thtg)
-	e1:SetOperation(c94620082.thop)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 	--special summon 
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(94620082,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,94620082)
-	e2:SetCondition(c94620082.spcond)
-	e2:SetCost(c94620082.spcost)
-	e2:SetTarget(c94620082.sptg)
-	e2:SetOperation(c94620082.spop)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.spcond)
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
-function c94620082.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>2 end
 end
-function c94620082.thfilter(c)
+function s.thfilter(c)
 	return c:IsSetCard(0x119) and c:IsAbleToHand()
 end
-function c94620082.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
 	Duel.ConfirmDecktop(tp,3)
 	local g=Duel.GetDecktopGroup(tp,3)
-	if g:IsExists(c94620082.thfilter,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(94620082,2)) then
+	if g:IsExists(s.thfilter,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local sg=g:FilterSelect(tp,c94620082.thfilter,1,1,nil)
+		local sg=g:FilterSelect(tp,s.thfilter,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg)
 		Duel.ShuffleHand(tp)
 	end
 	Duel.ShuffleDeck(tp)
 end
-function c94620082.costfilter(c)
-	return c:IsSetCard(0x119) and c:IsAbleToGraveAsCost()
+function s.costfilter(c)
+	return c:IsSetCard(0x119) and c:IsDiscardable()
 end
-function c94620082.spcond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c94620082.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+function s.spcond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
 end
-function c94620082.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(c94620082.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c94620082.costfilter,tp,LOCATION_HAND,0,1,1,c)
-	Duel.SendtoGrave(g,REASON_COST)
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD)
 end
-function c94620082.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
@@ -66,15 +65,15 @@ function c94620082.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	end
 end
-function c94620082.desfilter(c)
+function s.desfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
-function c94620082.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		local g=Duel.GetMatchingGroup(c94620082.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(94620082,3)) then
+		local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 			local sg=g:Select(tp,1,1,nil)
 			Duel.Destroy(sg,REASON_EFFECT)
