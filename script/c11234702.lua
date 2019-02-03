129e816ab68c14471cc3e0ddcf877262d15c872a
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-02-03 21:19:45 -0200

    Fixed "T.G. Screw Serpent"
    Fixed an incorrect interaction with Link monsters

--- script/c11234702.lua
+++ script/c11234702.lua
@@ -28,7 +28,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x27) and c:GetLevel()<=4 and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x27) and c:IsLevelBelow(4) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
