3a83046b53b20d294fad9a3f1b998d2528c7d3a7
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-02-21 20:30:18 -0300

    fix for Infinite Ignition Earthshaker
    Shouldn't be able to activate its effect to attach a token.

--- script/c100412009.lua
+++ script/c100412009.lua
@@ -62,9 +62,9 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
-function s.atchtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)	
-	if chk==0 then return e:GetHandler():IsType(TYPE_XYZ) end
+function s.atchtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local bc=e:GetHandler():GetBattleTarget()
+	if chk==0 then return e:GetHandler():IsType(TYPE_XYZ) and not bc:IsType(TYPE_TOKEN) end
 	Duel.SetTargetCard(bc)
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,bc,1,0,0)
 end
