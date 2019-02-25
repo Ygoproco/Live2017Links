f23da739fc0e6322e5483cf81d7198513d4680b4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-02-25 03:44:14 +0200

    Scrap Wyvern typo

--- script/c47363932.lua
+++ script/c47363932.lua
@@ -42,7 +42,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local tc=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tc,1,tp,LOCATION_GRAVE)
 	local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,0)
 	if #g>0 then
