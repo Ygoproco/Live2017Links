2b84631c8182f8ecd390213b7eda4fe52186e756
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-02-27 20:33:09 -0300

    Fix for "Runaway Railcar Flying Pegasus"
    Updated string called

--- script/c88875132.lua
+++ script/c88875132.lua
@@ -38,7 +38,8 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	Duel.Hint(HINT_SELECTMSG,tp,514)
+	--updated due to calling HINTMSG_FACEUP not working
 	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
