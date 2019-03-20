5c14bbf643347f0858685fc02d09dc2ce0c64775
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-03-20 17:17:38 -0300

    Fixed "B.E.S. Blaster Cannon Core"
    It was missing an EnableCounterPermit support

--- script/c101009023.lua
+++ script/c101009023.lua
@@ -3,6 +3,7 @@
 --Script by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
+	c:EnableCounterPermit(0x1f)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
