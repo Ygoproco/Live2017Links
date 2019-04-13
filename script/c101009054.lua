7a51a58c0855c85e9498c34e9919a290a95e7877
senpaizuri3 <senpaizuri3@gmail.com>
2019-04-13 10:13:26 -0400

    Fix "Fury of Fire"
    Formerly forced to Special Summon 2 monsters; currently able to summon 1 if wanted.

--- script/c101009054.lua
+++ script/c101009054.lua
@@ -6,10 +6,9 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -20,53 +19,45 @@ function s.initial_effect(c)
 	e2:SetValue(0x119)
 	c:RegisterEffect(e2)
 end
-function s.filter1(c,e,tp)
-	return c:IsSetCard(0x119) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
-end
-function s.filter2(c,g)
-	return g:IsExists(s.filter3,1,c,c:GetCode())
-end
-function s.filter3(c,code)
-	return c:GetCode()~=code
+function s.filter(c,e,tp,code)
+	return c:IsSetCard(0x119) and ((not code) or (not c:IsCode(code)))
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return false end
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return false end
-		local g=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_GRAVE+LOCATION_HAND,0,nil,e,tp)
-		return g:IsExists(s.filter2,1,nil,g)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_GRAVE+LOCATION_HAND)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
-	local g=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_GRAVE+LOCATION_HAND,0,nil,e,tp)
-	local dg=g:Filter(s.filter2,nil,g)
-	if g:GetCount()>=1 then
+	if not (Duel.GetLocationCount(tp,LOCATION_MZONE)>0) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE+LOCATION_HAND,0,nil,e,tp)
+	if g:GetCount()>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=dg:Select(tp,1,1,nil)
-		local tc1=sg:GetFirst()
-		dg:Remove(Card.IsCode,nil,tc1:GetCode())
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tc2=dg:Select(tp,1,1,nil):GetFirst()
-		Duel.SpecialSummonStep(tc1,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
-		Duel.SpecialSummonStep(tc2,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			tc1:RegisterEffect(e1,true)
-			local e2=Effect.CreateEffect(e:GetHandler())
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(EFFECT_DISABLE_EFFECT)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-			tc1:RegisterEffect(e2,true)
-			local e3=e1:Clone()
-			tc2:RegisterEffect(e3)
-			local e4=e2:Clone()
-			tc2:RegisterEffect(e4)
-		Duel.SpecialSummonComplete()
+		local tc1=g:Select(tp,1,1,nil):GetFirst()
+		g:Remove(Card.IsCode,nil,tc1:GetCode())
+		local tg=Group.FromCards(tc1)
+		if g:GetCount()>0 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			tc2=g:Select(tp,1,1,nil):GetFirst()
+			tg:AddCard(tc2)
+		end
+		for tc in aux.Next(tg) do
+			if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_DISABLE)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e1,true)
+				local e2=Effect.CreateEffect(e:GetHandler())
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetCode(EFFECT_DISABLE_EFFECT)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e2,true)
+				Duel.SpecialSummonComplete()
+			end
+		end
 	end
 	local e5=Effect.CreateEffect(e:GetHandler())
 	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
