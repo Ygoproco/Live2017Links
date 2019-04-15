27bc903517be0c3d26c713e383594bc5b70c13b6
EerieCode <eeriecode@gmail.com>
2019-04-15 22:05:35 +0200

    Fixed "Vision HERO Faris"

--- script/c100248010.lua
+++ script/c100248010.lua
@@ -59,6 +59,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
 	if tc and Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
 		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetCode(EFFECT_CHANGE_TYPE)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetReset(RESET_EVENT+0x1fc0000)
