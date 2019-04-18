a171160de90f99304f3ef544cb25b6b358a7bf02
EerieCode <eeriecode@gmail.com>
2019-04-18 22:13:55 +0200

    Fixed "You are in Danger!"

--- script/c101008085.lua
+++ script/c101008085.lua
@@ -19,27 +19,21 @@ function s.spfilter(c,e,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local rvg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and rvg:GetClassCount(Card.GetCode)>=3
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and rvg:GetClassCount(Card.GetCode)>=3 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_DECK)
+end
+function s.check(sg,e,tp)
+	return sg:GetClassCount(Card.GetCode)==#sg
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local fid=c:GetFieldID()
 	local rvg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
-	local rg=Group.CreateGroup()
-	for i=1,3 do
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-		local g=rvg:Select(tp,1,1,nil)
-		local tc=g:GetFirst()
-		rg:AddCard(tc)
-		rvg:Remove(Card.IsCode,nil,tc:GetCode())
-	end
+	local rg=aux.SelectUnselectGroup(rvg,e,tp,3,3,s.check,1,tp,HINTMSG_TARGET)
 	Duel.ConfirmCards(1-tp,rg)
 	local tg=rg:RandomSelect(1-tp,1)
 	local tc=tg:GetFirst()
 	if tc and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
-		--sg:RemoveCard(tc)
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1,fid)
 		local e1=Effect.CreateEffect(c)
@@ -49,17 +43,17 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCountLimit(1)
 		e1:SetLabel(fid)
 		e1:SetLabelObject(tc)
-		e1:SetCondition(s.drcon)
-		e1:SetOperation(s.drop)
+		e1:SetCondition(s.descon)
+		e1:SetOperation(s.desop)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
-end
-end	
-function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	end
+end 
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	return tc:GetFlagEffectLabel(id)==e:GetLabel()
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	Duel.Destroy(tc,REASON_EFFECT)
 end
