940534c1aa390a27c1e86ebffc3cbc9b4d6ba0c0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-05-02 22:23:23 +0300

    Various fixes (#1954)
    * "Endymion, the Mighty Master of Magic" fix
    
    Fixed interaction with "Stardust Dragon", etc.
    
    * "Tenyi Dragon Sthana" fix
    
    Should not be able to use its effect from the GY if it and a non-Effect Monster are destroyed at the same time.
    
    * "Salamangreat Foxy" fix
    
    Its GY effect should not be negatable with "Stardust Dragon", etc.
    
    * "Orcust Knightmare" fix
    
    Should not be able to use its GY effect after damage calculation and at the end of the Damage Step.
    
    * "Orcust Knightmare" update
    
    Added hint timing for the damage step.
    
    * "Yuki-Musume, the Ice Mayakashi" fix
    
    Her effect should be negatable by Ash Blossom.

--- script/c4055337.lua
+++ script/c4055337.lua
@@ -1,5 +1,5 @@
 --オルフェゴール・トロイメア
---Orcustrated Knightmare
+--Orcust Knightmare
 --Scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
@@ -26,7 +26,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetHintTiming(0,TIMING_END_PHASE)
+	e2:SetHintTiming(TIMING_DAMAGE_STEP,TIMING_DAMAGE_STEP+TIMING_END_PHASE)
 	e2:SetCondition(s.atkcon2)
 	c:RegisterEffect(e2)
 end
@@ -38,6 +38,7 @@ function s.atkcon1(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.atkcon2(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsPlayerAffectedByEffect(tp,CARD_ORPHEGEL_BABEL)
+		and (Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated())
 end
 function s.filter(c)
 	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_DARK) and not c:IsCode(id) and c:IsAbleToGrave()
