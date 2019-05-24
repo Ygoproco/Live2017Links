cd84e30053c0180b09aef2cf882f70cd5a389d7a
EerieCode <eeriecode@gmail.com>
2019-05-24 22:34:55 +0200

    Fixed "Gimmick Puppet Bisque Doll"
    Added missing reset and hint for protection effect

--- script/c100422036.lua
+++ script/c100422036.lua
@@ -37,11 +37,13 @@ end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	--cannot be target
 	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x83))
 	e1:SetValue(aux.tgoval)
+	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
