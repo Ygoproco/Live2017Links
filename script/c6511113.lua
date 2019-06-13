04defb20675f5e8ba8162dda65d7d755aaf34617
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-06-13 00:28:22 -0300

    small change for traptrix raflessia
    the flags were mixed.

--- script/c6511113.lua
+++ script/c6511113.lua
@@ -118,7 +118,7 @@ function s.filter2(c,e,tp,eg,ep,ev,re,r,rp)
 	else return false end
 end
 function s.filter3(c)
-	return c:GetFlagEffect(id) > 0
+	return c:GetFlagEffect(id+1) > 0
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
@@ -127,7 +127,7 @@ function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 		local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil,e,tp,eg,ep,ev,re,r,rp)
 		if #g>0 then
 			for tc in aux.Next(g) do
-				tc:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+				tc:RegisterFlagEffect(id+1,RESET_CHAIN,0,1)
 			end
 			return true
 		else
