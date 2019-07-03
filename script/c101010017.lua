367b13a9c36388a98bd1179d6386ef2ebfac6280
AlphaKretin <deewhydeeecks@gmail.com>
2019-07-03 11:38:03 +0930

    Fix "Aromage Laurier"
    Fix erroneous drawing category that allowed Ash Blossom to negate its Tuner effect

--- script/c101010017.lua
+++ script/c101010017.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
     --tuner
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_DRAW)
+    e2:SetCategory(CATEGORY_RECOVER)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
     e2:SetCode(EVENT_RECOVER)
     e2:SetRange(LOCATION_MZONE)
