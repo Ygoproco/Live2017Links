a1f436cde94d59f5997b2dd78b757b14a65acc98
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2019-07-13 01:55:14 -0400

    Updated "Wattrain"
    Previously forced the player to special summon multiple copies of the same card.

--- script/c101010060.lua
+++ script/c101010060.lua
@@ -55,19 +55,20 @@ function s.spfilter(c,e,tp)
 	return c:IsSetCard(0xe) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sprescon(sg,e,tp,mg)
-	return aux.ChkfMMZ(#sg)(sg,e,tp,mg) and sg:GetClassCount(Card.GetCode)==#sg
+	return sg:GetClassCount(Card.GetCode)>=#sg
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
-	local ct=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),g:GetClassCount(Card.GetCode))
-	if chk==0 then return ct>0 and aux.SelectUnselectGroup(g,e,tp,ct,ct,s.sprescon,chk) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
-	local ct=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),g:GetClassCount(Card.GetCode))
-	if ct<=0 then return end
-	local sg=aux.SelectUnselectGroup(g,e,tp,ct,ct,s.sprescon,1,tp,HINTMSG_SPSUMMON)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local tg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+	if #tg==0 or ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local g=aux.SelectUnselectGroup(tg,e,tp,1,ft,s.sprescon,1,tp,HINTMSG_SPSUMMON)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
