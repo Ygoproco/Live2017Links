048606853a75f8892b9cb9c95ae614c29c627fdd
Eronan <animeftw@live.co.uk>
2019-07-17 19:36:14 +1000

    Fix Update Jammer effect
    The activation of Update Jammer's attack gaining effect is not optional.

--- script/c88093706.lua
+++ script/c88093706.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
 	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCondition(s.condition)
