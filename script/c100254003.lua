3f62854cd04e283aebcd772621ce5e2bd6b6dfb7
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-07-18 13:36:47 -0300

    fix for Salamangreat Blaze Dragon
    missing end to close if on line 59

--- script/c100254003.lua
+++ script/c100254003.lua
@@ -60,6 +60,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 		local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
 		return #pg<=1 and Duel.GetLocationCountFromEx(tp,tp,e:GetHandler())>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,e:GetHandler(),pg)
+	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
