d3b3adaf48b03c0d66946a8fdc921322881f0948
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-08-08 23:41:56 +0300

    indentation

--- script/c100255021.lua
+++ script/c100255021.lua
@@ -2,7 +2,7 @@
 --Scripted by Hel
 local s,id=GetID()
 function s.initial_effect(c)
---negate
+	--negate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
