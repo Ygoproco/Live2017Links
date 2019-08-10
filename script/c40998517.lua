816e9b5dd7d3242caa9360eaa29c5a5a4899f2b4
mercury233 <me@mercury233.me>
2019-08-10 23:44:15 +0800

    Fix Fraudir, Generaid of the Sword
    Missing CATEGORY_DRAW

--- script/c40998517.lua
+++ script/c40998517.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	--destroy
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_DESTROY)
+    e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetCountLimit(1,id)
