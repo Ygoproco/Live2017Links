9b5b74d3d50f4e04f7d35f77287bae2bd8bae29f
Naim <naimsantos45@gmail.com>
2019-08-25 20:28:26 -0300

    Update c37561138.lua

--- script/c37561138.lua
+++ script/c37561138.lua
@@ -5,7 +5,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
