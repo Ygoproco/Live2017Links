c2a5f11422df4efaf04c31e226f01c47a0ae16ec
Naim <naimsantos45@gmail.com>
2019-09-13 15:54:54 -0300

    Updated additional Normal Summon effect
    Effect changed from mandatory trigger to optional trigger.

--- script/c42472002.lua
+++ script/c42472002.lua
@@ -38,7 +38,7 @@ function s.mtop(e,tp,eg,ep,ev,re,r,rp)
 	if not rc:IsSetCard(0x107a) then return end
 	--reg eff
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetOperation(s.effop)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
