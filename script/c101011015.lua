0523681c69faa6ac8dbf630fcb8f1c8747290f5c
Naim <naimsantos45@gmail.com>
2019-09-17 17:47:48 -0300

    Fixed Karakuri Toad mdl 4624 “Shirokunishi”
    Previously, the GY effect could not be activated

--- script/c101011015.lua
+++ script/c101011015.lua
@@ -44,7 +44,7 @@ function s.filter(c)
 end
 	--Activation legality
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) chkc:IsControler(tp) and s.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
