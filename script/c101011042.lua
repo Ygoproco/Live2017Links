833a0b1691985a2d61e1ac672db8dd0190add375
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-09-17 22:55:35 +0300

    "Wind Pegasus @ Ignister" fix
    - Moved a hint message
    - Now checks if it was the opponent that destroyed the card for the 2nd effect

--- script/c101011042.lua
+++ script/c101011042.lua
@@ -25,7 +25,6 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_DESTROYED)
 	e2:SetCountLimit(1,id+100)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetHintTiming(0,TIMING_END_PHASE)
 	e2:SetCondition(s.tdcon)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(s.tdtg)
@@ -36,12 +35,12 @@ s.listed_series={0x234}
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x234),tp,LOCATION_MZONE,0,1,nil)
 		and Duel.IsExistingMatchingCard(Card.IsType,tp,0,LOCATION_ONFIELD,1,nil,TYPE_SPELL+TYPE_TRAP) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsSetCard,0x234),tp,LOCATION_MZONE,0,1,nil)
 	if ct>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 		local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,0,LOCATION_ONFIELD,1,ct,nil,TYPE_SPELL+TYPE_TRAP)
 		if #g>0 then
 			Duel.Destroy(g,REASON_EFFECT)
@@ -49,7 +48,7 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.cfilter(c,tp)
-    return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:GetPreviousControler()==tp
+    return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:GetPreviousControler()==tp and c:GetReasonPlayer()==1-tp
 end
 function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsExists(s.cfilter,1,e:GetHandler(),tp)
