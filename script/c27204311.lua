c7e98142d7aca5deda236606bb19547a2dd16f8d
Naim <naimsantos45@gmail.com>
2019-09-20 11:32:40 -0300

    Small fix for "Nibiru, the Primal Being"
    Should still apply as much as possible of the effect while resolving

--- script/c27204311.lua
+++ script/c27204311.lua
@@ -58,12 +58,11 @@ function s.operationvaluedef(c)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
 	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsReleasable),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	if Duel.Release(g,REASON_EFFECT)==0 then return end
 	local og=Duel.GetOperatedGroup()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
-		and Duel.GetMZoneCount(1-tp,nil,tp)>0 then
+		and Duel.GetMZoneCount(1-tp,nil,tp)>0 and c:IsRelateToEffect(e) then
 		local atk=og:GetSum(s.operationvalueatk)
 			local def=og:GetSum(s.operationvaluedef)
 		if not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,0x4011,atk,def,11,RACE_ROCK,ATTRIBUTE_LIGHT,POS_FACEUP,1-tp) then return end
