6c590a791039593ff81b0cbf3881a755eec2caaf
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-09-23 23:40:13 +0300

    "Magicians' Combination" fix
    Fixed an issue where it would negate itself if the first effect was used in the same Chain Link as the card's activation.

--- script/c100413005.lua
+++ script/c100413005.lua
@@ -38,13 +38,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local ct=Duel.GetCurrentChain()
 	if ct==1 then return end
-	if s.negcon(e,tp,eg,ep,ev,re,r,rp)
-		and s.negcost(e,tp,eg,ep,ev,re,r,rp,0)
-		and s.negtg(e,tp,eg,ep,ev,re,r,rp,0)
+	if s.negcon(e,tp,eg,ep,ct-1,re,r,rp)
+		and s.negcost(e,tp,eg,ep,ct-1,re,r,rp,0)
+		and s.negtg(e,tp,eg,ep,ct-1,re,r,rp,0)
 		and Duel.SelectEffectYesNo(tp,e:GetHandler()) then
-		s.negcost(e,tp,eg,ep,ev,re,r,rp,1)
-		s.negtg(e,tp,eg,ep,ev,re,r,rp,1)
+		s.negcost(e,tp,eg,ep,ct-1,re,r,rp,1)
+		s.negtg(e,tp,eg,ep,ct-1,re,r,rp,1)
 		e:SetOperation(s.negop)
+		e:SetLabel(1)
 	end
 end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
@@ -63,7 +64,7 @@ function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,e,tp,ft) end
 	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,e,tp,ft)
-	e:SetLabel(g:GetFirst():GetCode())
+	e:SetLabelObject(g:GetFirst())
 	Duel.Release(g,REASON_COST)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -74,10 +75,16 @@ function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local ct=ev
+	if e:GetLabel()==1 then
+		e:SetLabel(0)
+		ct=Duel.GetCurrentChain()-1
+	end
+	local code=e:GetLabelObject():GetCode()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp,e:GetLabel())
+	local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp,code)
 	if #sg>0 and Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)>0 then
-		Duel.NegateEffect(ev)
+		Duel.NegateEffect(ct)
 	end
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
