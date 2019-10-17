37d56342dc680e15caa9bb5f18cea6102c6086b3
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2019-10-16 23:16:41 -0500

    Fixed Smile Action
    banish Effect only worked on turn 1

--- script/c100413048.lua
+++ script/c100413048.lua
@@ -1,72 +1,92 @@
 --スマイル・アクション
 --Smile Action
 --Scripted by AlphaKretin
+--Fixed by TOP
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetOperation(s.rmop)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--Negate attack
+	--negate attack
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_HANDES)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e2:SetRange(LOCATION_FZONE)
-	e2:SetOperation(s.thop)
+	e2:SetTarget(s.atktg)
+	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
+	e1:SetLabelObject(e2)
 end
 function s.rmfilter(c)
 	return c:IsType(TYPE_SPELL) and c:IsAbleToRemove()
 end
-function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-	local rg=Group.CreateGroup()
-	for p=tp,1-tp do
-		if Duel.IsExistingMatchingCard(s.rmfilter,p,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(p,aux.Stringid(id,1)) then
-			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_REMOVE)
-			local g=Duel.SelectMatchingCard(p,s.rmfilter,p,LOCATION_GRAVE,0,1,5,nil)
-			rg:Merge(g)
-		end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g1=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_GRAVE,0,nil)
+	local g2=Duel.GetMatchingGroup(s.rmfilter,tp,0,LOCATION_GRAVE,nil)
+	local sg=Group.CreateGroup()
+	if g1:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local sg1=g1:Select(tp,1,5,nil)
+		sg:Merge(sg1)
 	end
-	if #rg>0 and Duel.Remove(rg,POS_FACEDOWN,REASON_EFFECT)>0 then
-		local og=Duel.GetOperatedGroup()
-		for tc in aux.Next(og) do
-			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
+	if g2:GetCount()>0 and Duel.SelectYesNo(1-tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_REMOVE)
+		local sg2=g2:Select(1-tp,1,5,nil)
+		sg:Merge(sg2)
+	end
+	if sg:GetCount()>0 then
+		Duel.Remove(sg,POS_FACEDOWN,REASON_EFFECT)
+		local rg=sg:Filter(Card.IsLocation,nil,LOCATION_REMOVED)
+		rg:KeepAlive()
+		e:GetLabelObject():SetLabelObject(rg)
+		local rc=rg:GetFirst()
+		while rc do
+			rc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+			rc=rg:GetNext()
 		end
 	end
 end
-function s.thfilter(c)
-	return c:GetFlagEffect(id)~=0 and c:IsAbleToHand()
+function s.thfilter(c,p)
+	return c:GetFlagEffect(id)>0 and c:IsControler(p) and c:IsAbleToHand()
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local at=Duel.GetAttacker()
-	local p=1-at:GetControler()
-	if Duel.IsExistingMatchingCard(s.thfilter,p,LOCATION_REMOVED,0,1,nil) and Duel.SelectYesNo(p,aux.Stringid(id,2)) then
-		local g=Duel.GetMatchingGroup(s.thfilter,p,LOCATION_REMOVED,0,nil)
-		local tg=g:RandomSelect(p,1)
-		Duel.SendtoHand(tg,nil,REASON_EFFECT)
-		local tc=tg:GetFirst()
-		if tc:IsDiscardable(REASON_EFFECT) and Duel.SelectYesNo(p,aux.Stringid(id,3)) then
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,1-rp,LOCATION_REMOVED)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,1,1-rp,LOCATION_HAND)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject():Filter(s.thfilter,nil,1-rp)
+	if g:GetCount()>0 and Duel.SelectYesNo(1-rp,aux.Stringid(id,2)) then
+		local flag=false
+		local sg=g:RandomSelect(rp,1)
+		local sc=sg:GetFirst()
+		if Duel.SendtoHand(sc,nil,REASON_EFFECT)~=0 and sc:IsLocation(LOCATION_HAND) and sc:IsControler(1-rp)
+			and sc:IsDiscardable() and Duel.SelectYesNo(1-rp,aux.Stringid(id,3)) then
+			flag=true
 			Duel.BreakEffect()
-			Duel.SendtoGrave(tc,nil,REASON_EFFECT+REASON_DISCARD)
+			Duel.SendtoGrave(sc,REASON_EFFECT+REASON_DISCARD)
 			Duel.NegateAttack()
-		else
-			--double damage
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-			e1:SetOperation(s.damop)
+		end
+		if flag==false then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_CHANGE_DAMAGE)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetTargetRange(1,0)
+			e1:SetValue(s.damval)
 			e1:SetReset(RESET_PHASE+PHASE_END)
-			e1:SetLabel(p)
-			Duel.RegisterEffect(e1,p)
+			Duel.RegisterEffect(e1,1-rp)
 		end
 	end
 end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	if ep==e:GetLabel() then
-		Duel.DoubleBattleDamage(ep)
-	end
+function s.damval(e,re,dam,r,rp,rc)
+	if bit.band(r,REASON_BATTLE)~=0 then
+		return dam*2
+	else return dam end
 end
