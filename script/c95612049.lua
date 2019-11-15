6669c36c7c8f02cc2a373c97373d8f6e0e01ec7b
edo9300 <edoardo762@gmail.com>
2019-11-15 22:28:59 +0100

    Indentation

--- script/c95612049.lua
+++ script/c95612049.lua
@@ -4,8 +4,8 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Ritual Summon
 	local e1=aux.CreateRitualProc(c,RITPROC_GREATER,aux.FilterBoolFunction(Card.IsCode,46427957,72426662),nil,nil,nil,nil,s.mfilter,nil,LOCATION_HAND+LOCATION_DECK)
-    e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
-    c:RegisterEffect(e1)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
 end
 function s.mfilter(c)
 	return c:IsLocation(LOCATION_HAND) and c:IsType(TYPE_RITUAL)
