bb594542e1f430efc4c1a70914f25dde3ea5fcc0
edo9300 <edoardo762@gmail.com>
2019-11-16 20:00:35 +0100

    Missing checks in vendread reunion

--- script/c2266498.lua
+++ script/c2266498.lua
@@ -2,10 +2,24 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--special summon
-	local e1=aux.AddRitualProc(c,RITPROC_EQUAL,s.cfilter,nil,nil,s.extrafil,nil,s.filter,nil,nil,s.customoperation)
+	local e1=aux.CreateRitualProc(c,RITPROC_EQUAL,s.cfilter,nil,nil,s.extrafil,nil,s.filter,nil,s.ritcheck,s.customoperation)
+	e1:SetTarget(s.registerloccount(e1:GetTarget()))
+	e1:SetOperation(s.registerloccount(e1:GetOperation()))
+	c:RegisterEffect(e1)
+end
+function s.registerloccount(func)
+	return function(e,tp,...)
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		if Duel.IsPlayerCanSpecialSummonCount(tp,2) then ft=0 end
+		if ft>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+		e:SetLabel(ft)
+		local res=func(e,tp,...)
+		e:SetLabel()
+		return res
+	end
 end
 function s.cfilter(c,e,tp,m,ft)
-	return c:IsSetCard(0x106) and not c:IsPublic()
+	return e:GetLabel()>0 and c:IsSetCard(0x106) and c:IsPublic()
 end
 function s.filter(c,e,tp)
 	return c:IsFaceup() and c:IsSetCard(0x106) and Duel.IsPlayerCanRelease(tp,c)
@@ -13,6 +27,10 @@ function s.filter(c,e,tp)
 end
 function s.extrafil(e,tp,eg,ep,ev,re,r,rp)
 	Duel.GetMatchingGroup(s.filter,tp,LOCATION_REMOVED,0,nil,e,tp)
+end
+function s.ritcheck(e,tp,g,sc)
+	local count=#g
+	return g:GetClassCount(Card.GetCode)==count and count <=e:GetLabel()
 end
 function s.customoperation(mg,e,tp,eg,ep,ev,re,r,rp,tc)
 	Duel.ConfirmCards(1-tp,tc)
