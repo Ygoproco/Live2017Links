8d03b30f7562321ef74973542496364875ff426e
NaimSantos <naimsantos45@gmail.com>
2019-11-16 08:05:47 -0300

    Updated condition function used with Tenyi Spirit - Sahasrara

--- script/c100257096.lua
+++ script/c100257096.lua
@@ -34,7 +34,7 @@ function s.initial_effect(c)
 	e3:SetOperation(s.operation)
 	c:RegisterEffect(e3)
 end
-function s.con(e,c)
+function s.con(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsNonEffectMonster),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.filter(c)
