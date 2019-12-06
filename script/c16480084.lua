6c75ac0661d2ff56eba42c1747329dbff36ca326
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2019-12-06 16:28:15 -0600

    Fixed Evolsaur Pelta
    Fixed effect on summon
    it was missing the initial effect

--- script/c16480084.lua
+++ script/c16480084.lua
@@ -1,41 +1,50 @@
 --エヴォルダー・ペルタ
-function c16480084.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_ATKCHANGE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCondition(aux.evospcon)
+    e1:SetOperation(s.regeff)
+    c:RegisterEffect(e1)
 end
-function c16480084.evolreg(c)
-	--defup
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_DEFENSE)
-	e1:SetValue(500)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
-	c:RegisterEffect(e1)
-	--search
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(16480084,0))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_BATTLE_DESTROYED)
-	e2:SetCondition(c16480084.schcon)
-	e2:SetTarget(c16480084.schtg)
-	e2:SetOperation(c16480084.schop)
-	e2:SetReset(RESET_EVENT+0x7b0000)
-	c:RegisterEffect(e2)
+function s.regeff(c)
+    --defup
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_DEFENSE)
+    e1:SetValue(500)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+    c:RegisterEffect(e1)
+    --search
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_BATTLE_DESTROYED)
+    e2:SetCondition(s.schcon)
+    e2:SetTarget(s.schtg)
+    e2:SetOperation(s.schop)
+    e2:SetReset(RESETS_STANDARD-(RESET_DISABLE+RESET_TURN_SET+RESET_LEAVE+RESET_TOFIELD))
+    c:RegisterEffect(e2)
 end
-function c16480084.schcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
+function s.schcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
 end
-function c16480084.sfilter(c)
-	return c:IsSetCard(0x304e) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+function s.sfilter(c)
+    return c:IsSetCard(0x304e) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-function c16480084.schtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c16480084.sfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function s.schtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.sfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c16480084.schop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c16480084.sfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+function s.schop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.sfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if g:GetCount()>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end
