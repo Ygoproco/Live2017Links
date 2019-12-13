a8aaf4dc70e430143fc72f09674d2f5a48eb3694
ahtelel <32773013+ahtelel@users.noreply.github.com>
2019-12-13 09:58:35 +0700

    Fix Galaxy-Eyes Afterglow Dragon
    Fixed various filter bugs and interactions

--- script/c100259029.lua
+++ script/c100259029.lua
@@ -21,6 +21,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id+100)
 	e2:SetCondition(s.atcon)
 	e2:SetTarget(s.attg)
 	e2:SetOperation(s.atop)
@@ -50,7 +51,10 @@ function s.atcon(e,tp,eg,ep,ev,re,r,rp)
 		and c:IsPreviousLocation(LOCATION_OVERLAY)
 end
 function s.cfilter2(c)
-	return c:IsFaceup() and c:IsSetCard(0x48) and c:IsType(TYPE_XYZ)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ)
+end
+function s.cfilter3(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x48)
 end
 function s.attg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,CARD_GALAXYEYES_P_DRAGON) end
@@ -58,21 +62,26 @@ end
 function s.atop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local ph=Duel.GetCurrentPhase()
-	local tg=Duel.GetMatchingGroup(s.cfilter2,tp,LOCATION_MZONE,0,1,nil)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil)
+	local tg=Duel.GetMatchingGroup(s.cfilter2,tp,LOCATION_MZONE,0,1,nil,e)
+	local sg=Duel.GetMatchingGroup(s.cfilter3,tp,LOCATION_MZONE,0,1,nil)
+	local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,CARD_GALAXYEYES_P_DRAGON)
 	local tc=g:GetFirst()
 	local op=0
-		if #tg>0 and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
+		if #tg>0 and (tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0) then
+		op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
 		elseif #tg>0 then op=0
 		else op=1 end
 		if op==0 then
-			local oc=tg:Select(tp,1,1,nil):GetFirst()
+			local oc=tg:Select(tp,1,1,nil,e):GetFirst()
+			if oc:IsImmuneToEffect(e) then Duel.SendtoGrave(tc,REASON_RULE) end
+			if oc and tc:IsRelateToEffect(e) and oc:IsFaceup() and tc:IsRelateToEffect(e) then
 			Duel.Overlay(oc,tc)
-		else
+			end
+		else 
 			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 		end
 	if ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE then
-		local sc=tg:GetFirst()
+		local sc=sg:GetFirst()
 		while sc do
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
@@ -80,7 +89,7 @@ function s.atop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			e1:SetValue(sc:GetAttack()*2)
 			sc:RegisterEffect(e1)
-			sc=tg:GetNext()
+			sc=sg:GetNext()
 		end
 	end
 end
