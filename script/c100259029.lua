7699e8523277e4dd35a8f54223de93f20092f77c
NaimSantos <naimsantos45@gmail.com>
2019-12-13 20:42:47 -0300

    Added strings to "Galaxy-Eyes Afterglow Dragon"

--- script/c100259029.lua
+++ script/c100259029.lua
@@ -3,7 +3,7 @@
 --Scripted by ahtelel
 local s,id=GetID()
 function s.initial_effect(c)
-	--sp summon 1
+	--special summon from hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--sp summon 2/attach
+	--special summon or attach
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
