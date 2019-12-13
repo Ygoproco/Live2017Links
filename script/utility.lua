2ff681502aac11bf6114aa1094d9315fe0214feb
EerieCode <eeriecode@gmail.com>
2019-12-13 09:30:55 +0100

    Updated auxiliary functions

--- script/utility.lua
+++ script/utility.lua
@@ -1485,15 +1485,18 @@ function Auxiliary.ToHandOrElse(card,player,check,oper,str,...)
             opt=Duel.SelectOption(player,str)+1
         end
         if opt==0 then
-            Duel.SendtoHand(card,nil,REASON_EFFECT)
-            Duel.ConfirmCards(1-player,card)
+            local res=Duel.SendtoHand(card,nil,REASON_EFFECT)
+            if res~=0 then
+				Duel.ConfirmCards(1-player,card)
+	    end
+	    return res
         else
-            oper(card,...)
+            return oper(card,...)
         end
     end
 end
 function Auxiliary.thoeSend(card)
-    Duel.SendtoGrave(card,REASON_EFFECT)
+    return Duel.SendtoGrave(card,REASON_EFFECT)
 end
 function loadutility(file)
 	local f1 = loadfile("expansions/live2017links/script/"..file)
