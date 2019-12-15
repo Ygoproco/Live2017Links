e53a293c08c33bbf01ac0f2978fade1bbfcaec19
lrhel <lrhel@users.noreply.github.com>
2019-12-15 10:58:19 +0100

    Bug Fix Borrelend (#2311)
    Player can now target Borrelend Dragon in the GY/Banished zone with Monster Effect

--- script/c100259045.lua
+++ script/c100259045.lua
@@ -9,6 +9,8 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(s.etg)
 	c:RegisterEffect(e1)
 	--indes
