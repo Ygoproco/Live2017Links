dd53a3d30a018f121c82b381cb44e39c74a4be53
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2019-12-22 04:31:14 -0500

    Updated "Morph King Stygi-Gel" (#2317)
    * Updated "Morph King Stygi-Gel"
    
    Made the following changes:
    * Shouldn't be able to special summon monsters with same level as "Morph King Stygi-Gel" with ignition effect
    * Gaining LP wasn't working
    * Extra deck restriction wasn't working

--- script/c100229008.lua
+++ script/c100229008.lua
@@ -1,4 +1,4 @@
---変容王ヘル・ゲル
+--変容王 ヘル・ゲル
 --Morph King Stygi-Gel
 --Scripted by AlphaKretin
 local s,id=GetID()
@@ -37,7 +37,7 @@ function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.lvfilter(chkc,lv) end
 	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,0,1,nil,lv) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,0,1,1,nil,lv)
+	local tc=Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,0,1,1,nil,lv):GetFirst()
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,tc:GetLevel()*200)
 end
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
@@ -52,7 +52,7 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
 		if (c:IsLevel(lv)) then
-			Duel.Recover(tp,lv*200)
+			Duel.Recover(tp,lv*200,REASON_EFFECT)
 		end
 	end
 	local e2=Effect.CreateEffect(c)
@@ -64,11 +64,11 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetTarget(s.splimit)
 	Duel.RegisterEffect(e2,tp)
 end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return() sumtype&SUMMON_TYPE_SYNCHRO)~=SUMMON_TYPE_SYNCHRO
+function s.splimit(e,c)
+	return not c:IsType(TYPE_SYNCHRO) and c:IsLocation(LOCATION_EXTRA)
 end
 function s.spfilter(c,e,tp,lv)
-	return c:IsRace(RACE_FIEND) and c:IsHasLevel() and c:IsLevelBelow(lv) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsRace(RACE_FIEND) and c:IsHasLevel() and c:IsLevelBelow(lv-1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local lv=e:GetHandler():GetLevel()
