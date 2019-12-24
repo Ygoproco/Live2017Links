8a84453732efd32d6df9f0e3d6fbee09aa13d263
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-12-24 17:45:47 +0200

    "Mask Change" fix
    Should now behave properly when using MR 2020.

--- script/c21143940.lua
+++ script/c21143940.lua
@@ -1,5 +1,7 @@
 --マスク・チェンジ
-function c21143940.initial_effect(c)
+--Mask Change
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -7,36 +9,37 @@ function c21143940.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetTarget(c21143940.target)
-	e1:SetOperation(c21143940.activate)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function c21143940.tfilter(c,att,e,tp,mc)
+function s.tfilter(c,att,e,tp,mc)
 	return c:IsSetCard(0xa008) and c:IsAttribute(att) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
-function c21143940.filter(c,e,tp)
+function s.filter(c,e,tp)
 	return c:IsFaceup() and c:IsSetCard(0x8)
-		and Duel.IsExistingMatchingCard(c21143940.tfilter,tp,LOCATION_EXTRA,0,1,nil,c:GetAttribute(),e,tp,c)
+		and Duel.IsExistingMatchingCard(s.tfilter,tp,LOCATION_EXTRA,0,1,nil,c:GetAttribute(),e,tp,c)
 end
-function c21143940.chkfilter(c,att)
+function s.chkfilter(c,att)
 	return c:IsFaceup() and c:IsSetCard(0x8) and c:IsAttribute(att)
 end
-function c21143940.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c21143940.chkfilter(chkc,e:GetLabel()) end
-	if chk==0 then return Duel.IsExistingTarget(c21143940.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.chkfilter(chkc,e:GetLabel()) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectTarget(tp,c21143940.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 	e:SetLabel(g:GetFirst():GetAttribute())
 end
-function c21143940.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if not tc:IsRelateToEffect(e) then return end
 	local att=tc:GetAttribute()
 	if Duel.SendtoGrave(tc,REASON_EFFECT)==0 then return end
-	if Duel.GetLocationCountFromEx(tp)<=0 then return end
+	local g=Duel.GetMatchingGroup(s.tfilter,tp,LOCATION_EXTRA,0,nil,att,e,tp)
+	if #g==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=Duel.SelectMatchingCard(tp,c21143940.tfilter,tp,LOCATION_EXTRA,0,1,1,nil,att,e,tp)
+	local sg=g:Select(tp,1,1,nil)
 	if sg:GetCount()>0 then
 		Duel.BreakEffect()
 		Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)
