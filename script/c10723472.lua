683ed868090b31febbd50c95b44b7f4b4f27be5e
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2019-12-25 02:56:36 -0500

    Updated "Burning Soul"
    Wasn't able to add a card from GY to hand, also will now confirm the added card.

--- script/c10723472.lua
+++ script/c10723472.lua
@@ -38,7 +38,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
-	local g=Duel.SelectMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,1,nil) 
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil) 
 	if #g>0 then
 		if Duel.SendtoHand(g,tp,REASON_EFFECT)>0 then
 			local mg=Duel.GetMatchingGroup(Card.IsOnField,tp,LOCATION_MZONE,0,nil)
@@ -50,6 +50,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			end
 		end
 	end
+	Duel.ConfirmCards(1-tp,g)
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
