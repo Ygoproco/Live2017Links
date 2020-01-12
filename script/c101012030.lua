b4dfef0e29a79bd8b922d936923c2c78a3bc7cee
senpaizuri3 <senpaizuri3@gmail.com>
2020-01-12 16:06:05 -0500

    Update "Beast Lord Barbaros" for "Lair of Darkness" interactions
    Now you can use its effect to Special Summon itself by tributing 1 monster opponent controls affected by Lair (as well as appropriate monsters you control, if needed).

--- script/c101012030.lua
+++ script/c101012030.lua
@@ -1,6 +1,6 @@
 --獣神王バルバロス
 --Beast Lord Barbaros
---Scripted by AlphaKretin
+--Scripted by AlphaKretin; rework for Lair by senpaizuri
 local s,id=GetID()
 function s.initial_effect(c)
 	--special summon
@@ -41,15 +41,48 @@ end
 function s.rescheck(c,lv)
 	return lv-c:GetLevel()>=8
 end
-function s.cfilter(c)
-	return c:IsReleasable() and c:IsHasLevel()
+function s.cfilter(c,tp)
+	return c:IsReleasable() and c:IsHasLevel() and (c:IsControler(tp) or (c:IsFaceup() and c:IsHasEffect(59160188)))
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	--if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsHasLevel,1,false,s.rescon,nil) end
 	--local rg=Duel.SelectReleaseGroupCost(tp,Card.IsHasLevel,1,99,false,s.rescon,nil)
-	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil)
-	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,1,#g,s.rescon,0) end
-	local rg=aux.SelectUnselectGroup(g,e,tp,1,#g,s.rescon,1,tp,HINTMSG_RELEASE)
+	--local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil)
+	--if chk==0 then return aux.SelectUnselectGroup(g,e,tp,1,#g,s.rescon,0) end
+	--local rg=aux.SelectUnselectGroup(g,e,tp,1,#g,s.rescon,1,tp,HINTMSG_RELEASE)
+	if chk==0 then
+		local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil,tp)
+		local res=g:CheckWithSumGreater(Card.GetLevel,8)
+		if Duel.IsExistingMatchingCard(s.cfilter,tp,0,LOCATION_MZONE,1,nil,tp) then
+			for tc in aux.Next(Duel.GetMatchingGroup(s.cfilter,tp,0,LOCATION_MZONE,nil,tp)) do
+				g:AddCard(tc)
+				res=res or g:CheckWithSumGreater(Card.GetLevel,8)
+				g:RemoveCard(tc)
+			end
+		end
+		return res
+	end
+	local rg=Group.CreateGroup()
+	while not rg:CheckWithSumGreater(Card.GetLevel,8) do
+		local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tp)
+		if rg:IsExists(Card.IsControler,1,nil,1-tp) then g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil,tp) end
+		local mg=g:Filter(Auxiliary.SelectUnselectLoop,rg,rg,g,e,tp,1,99,s.rescon)
+		if mg:GetCount()<=0 or rg:GetCount()>=99 then break end
+		Duel.Hint(HINT_SELECTMrg,tp,HINTMrg_RELEASE)
+		local tc=mg:SelectUnselect(rg,tp)
+		if not tc then break end
+		if rg:IsContains(tc) then
+			rg:RemoveCard(tc)
+		else
+			rg:AddCard(tc)
+		end
+	end
+	--lair register
+	if rg:IsExists(Card.IsControler,1,nil,1-tp) then
+		local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+		Duel.Hint(HINT_CARD,0,fc:GetCode())
+		fc:RegisterFlagEffect(59160188,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+	end
 	Duel.Release(rg,REASON_COST)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
