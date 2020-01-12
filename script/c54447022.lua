dc97556443109be0bad698a33a55959a1b7a6f1b
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2020-01-11 20:03:59 -0600

    Fixed Soul Charge
    same as Scrap Lube

--- script/c54447022.lua
+++ script/c54447022.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCurrentPhase()~=PHASE_MAIN2 end
+	if chk==0 then return Duel.GetActivityCount(tp,ACTIVITY_BATTLE_PHASE)==0 end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_BP)
