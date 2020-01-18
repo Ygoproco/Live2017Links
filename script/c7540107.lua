aff5ce724b6cbed47eabcca83bf150ac39e29f70
Naim <naimsantos45@gmail.com>
2020-01-18 16:50:16 -0300

    Update c7540107.lua

--- script/c7540107.lua
+++ script/c7540107.lua
@@ -49,6 +49,7 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
 	local c=e:GetHandler()
+	local tc=g:GetFirst()
 	for tc in aux.Next(g) do
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
