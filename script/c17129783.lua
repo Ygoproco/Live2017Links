e5e68585291f356a8beaf2e9905da67219cd8877
NaimSantos <naimsantos45@gmail.com>
2020-02-05 08:30:16 -0300

    Added strings for cards that grant an additional Normal/Tribute Summon
    Constellar Leonis
    Mithra the Thunder Vassal, also includes a client hint
    Karakuri Komachi mdl 224 "Ninishi"
    Eidos the Underworld Squire, also includes a client hint
    Swap Frog
    Ritual Beast Tamer Elder
    Aromage Jasmine
    Atlantean Heavy Infantry

---
+++ script/c17129783.lua
@@ -0,0 +1,14 @@
+--セイクリッド・レオニス
+--Constellar Leonis
+local s,id=GetID()
+function s.initial_effect(c)
+	--extra summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x53))
+	c:RegisterEffect(e1)
+end
---
+++ script/c24621460.lua
@@ -0,0 +1,33 @@
+--カラクリ小町 弐弐四
+--Karakuri Komachi mdl 224 "Ninishi"
+local s,id=GetID()
+function s.initial_effect(c)
+	--must attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_MUST_ATTACK)
+	c:RegisterEffect(e1)
+	--pos change
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_BE_BATTLE_TARGET)
+	e2:SetOperation(s.posop)
+	c:RegisterEffect(e2)
+	--extra summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e3:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x11))
+	c:RegisterEffect(e3)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE,0,POS_FACEUP_ATTACK,0)
+	end
+end
