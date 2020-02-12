6e4a6eadbe0f44de0792c7e1fdc91148fe85703b
senpaizuri3 <senpaizuri3@gmail.com>
2020-02-11 23:24:43 -0500

    Small fix "Machina Defense Region"
    Now the effect no longer affects cards without levels, such as Link or Xyz Monsters

--- script/c100310024.lua
+++ script/c100310024.lua
@@ -48,10 +48,10 @@ function s.cannottgcon(e)
 	return Duel.IsExistingMatchingCard(s.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
 end
 function s.cannottgeff(e,c)
-	return c:GetLevel()<=6
+	return c:IsLevelBelow(6)
 end
 function s.cannotatk(e,c)
-	return c:IsFaceup() and c:GetLevel()<=6
+	return c:IsFaceup() and c:IsLevelBelow(6)
 end
 function s.tohandconfilter(c,tp)
 	return c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY)
