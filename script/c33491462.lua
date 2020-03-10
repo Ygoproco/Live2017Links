95274b26876e0546f90c2164bdeb81eaa81b3524
NaimSantos <naimsantos45@gmail.com>
2020-03-10 17:23:41 -0300

    Fix "Snowflower Sprite Snowdrop"
    Fixed a problem where it sometimes could not be activated while the player had only 1 monster zone available

--- script/c33491462.lua
+++ script/c33491462.lua
@@ -45,7 +45,7 @@ end
 	--Activation legality
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,e:GetHandler(),e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND)
