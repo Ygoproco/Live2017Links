1feb05b10fcfd815d8f8da35bea9f1251a853730
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-03-23 04:00:56 +0200

    "Field Barrier" fix
    Should only protect cards in the Field Zone.

---
+++ script/c7153114.lua
@@ -0,0 +1,32 @@
+--フィールドバリア
+--Field Barrier
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--indestructable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_FZONE,LOCATION_FZONE)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--cannot activate
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,1)
+	e3:SetValue(s.filter)
+	c:RegisterEffect(e3)
+end
+function s.filter(e,re,tp)
+	return re:GetHandler():IsType(TYPE_FIELD) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
