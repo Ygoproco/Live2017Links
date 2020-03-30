97be22a953b1651561f71601dc076c53a426ea1f
NaimSantos <naimsantos45@gmail.com>
2020-03-30 12:08:01 -0300

    Updated errata versions

--- script/c88264978.lua
+++ script/c88264978.lua
@@ -1,30 +1,29 @@
 --レッドアイズ・ダークネスメタルドラゴン
+--Red-Eyes Darkness Metal Dragon, pre-errata, TCG
 local s,id=GetID()
 function s.initial_effect(c)
-	--spsummon proc
+	--special summon procedure
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.hspcon)
 	e1:SetOperation(s.hspop)
 	c:RegisterEffect(e1)
-	--spsummon
+	--special summon a dragon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetCountLimit(1,id+1)
+	e2:SetCountLimit(1)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
 function s.spfilter(c,ft)
-	return c:IsFaceup() and c:IsRace(RACE_DRAGON) and c:IsAbleToRemoveAsCost()
-		and (ft>0 or c:GetSequence()<5)
+	return c:IsFaceup() and c:IsRace(RACE_DRAGON) and c:IsAbleToRemoveAsCost() and (ft>0 or c:GetSequence()<5)
 end
 function s.hspcon(e,c)
 	if c==nil then return true end
